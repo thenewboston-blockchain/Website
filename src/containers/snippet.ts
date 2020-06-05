338c5d48c34ed06416f2d76830cc1f3e5153056f
Bucky Roberts <buckyroberts@gmail.com>
2020-06-05 16:11:54 -0700

    Syntax highlighter (#1)
    * Syntax highlighting
    
    * Syntax highlighting
    
    * yarn > npm

---
+++ src/containers/snippet.ts
@@ -0,0 +1,31 @@
+const codeString = `
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+from thenewboston.models.network_node import NetworkNode
+
+
+class Bank(NetworkNode):
+    trust = models.DecimalField(
+        decimal_places=2,
+        default=0,
+        max_digits=5,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
+
+    class Meta:
+        default_related_name = 'banks'
+
+    def __str__(self):
+        return (
+            f'ID: {self.id} | '
+            f'IP Address: {self.ip_address} | '
+            f'Trust: {self.trust} | '
+            f'Version: {self.version}'
+        )
+  `;
+
+export default codeString;
