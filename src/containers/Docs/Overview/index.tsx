5c690b1c6e6578739d82963900b94ecd86dd661e
Bucky <buckyroberts@gmail.com>
2020-06-29 16:11:38 -0700

    Overview

--- src/containers/Docs/Overview/index.tsx
+++ src/containers/Docs/Overview/index.tsx
@@ -61,21 +61,21 @@ const Overview = () => {
         <img alt="overview diagram" className="overview-diagram" src={OverviewDiagram} />
       </div>
       <p>
-        All transactions on the network will begin with a user account. The owner of the account will create a
-        transaction, indicating the amount of points they would like to send along with the recipient, and send that
-        transaction to their bank. The bank will then forward the transaction along to the validator and upon successful
-        validation of the transaction, append it onto the transaction ledger and update the network balance sheet. There
-        are several key differences between the network structure outlined above and the traditional Blockchain
-        architecture regarding the creation of a distributed public ledger.
+        All transactions on the network will begin with a user account. The owner of the account will create a block
+        (list of transactions) indicating the amount of points they would like to send to each recipient and then send
+        that block to their bank. The bank will then forward the block along to the validator and upon successful
+        validation of the transaction, append it onto blockchain while updating account balances. There are several key
+        differences between the network structure outlined above and the traditional Blockchain architecture regarding
+        the creation of a distributed public ledger.
       </p>
       <p>
         An inherent defect in the modern Blockchain architecture is the inefficient composition of blocks. Blocks in the
         Bitcoin Blockchain are composed of multiple unrelated transactions. This indicates that within any given block,
         the earliest transactions would have experienced significant delays as later transactions continued to
         accumulate until the entire block was verified. If one was only considering performance of the Blockchain and
-        nothing else, they would arrive at the conclusion that the fastest Blockchain would not have any blocks.
-        Transactions would instead be validated immediatley as they were received rather than waiting to be bundled
-        along with additional unrelated transactions.
+        nothing else, they would arrive at the conclusion that in an ideal architecture transactions would be validated
+        immediately as they were received rather than waiting to be bundled along with additional unrelated
+        transactions.
       </p>
       <p>
         By removing the inclusion of unrelated transactions into blocks we are able to substantially reduce the average
@@ -84,8 +84,8 @@ const Overview = () => {
         detail in the Blocks section of the paper.
       </p>
       <p>
-        These improvements, along with others discussed throughout this paper, when implemented correctly reduce the
-        overall energy consumption and processing power required by the network. This increased efficiency not only
+        These improvements, along with others discussed throughout this documentation, when implemented correctly reduce
+        the overall energy consumption and processing power required by the network. This increased efficiency not only
         enables transactions between users to be verified within seconds (or less), but allows developers to utilize
         more popular programming languages (Python, JavaScript, etc...) towards expediting early development while
         encouraging rapid growth and innovation.
