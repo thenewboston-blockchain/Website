8e5dccc0f296197c2486c63dfa3715889d46b081
Bucky <buckyroberts@gmail.com>
2020-06-30 15:56:34 -0700

    Overview updates

--- src/containers/Docs/Overview/index.tsx
+++ src/containers/Docs/Overview/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 import OverviewDiagram from './Overview.png';
 
@@ -29,6 +30,10 @@ const Overview = () => {
       <tr>
         <td>Bank</td>
         <td>a network server that has several responsibilities such as electing a validator and routing blocks</td>
+      </tr>
+      <tr>
+        <td>Bank Block</td>
+        <td>a block that has been signed by a bank</td>
       </tr>
       <tr>
         <td>Validator</td>
@@ -72,33 +77,33 @@ const Overview = () => {
 
       <p>
         All transactions on the network will begin with a user account. The owner of the account will create a block
-        (list of transactions) indicating the amount of points they would like to send to each recipient and then send
-        that block to their bank. The bank will then forward the block along to the validator and upon successful
-        validation of the transaction, append it onto blockchain while updating account balances. There are several key
-        differences between the network structure outlined above and the traditional Blockchain architecture regarding
-        the creation of a distributed public ledger.
+        (group of transactions) indicating the amount of points they would like to send to each recipient and then send
+        that block to their bank. The bank will then forward the block (now a bank block) along to the validator and
+        upon successful validation of the transaction, append it onto blockchain while updating account balances. There
+        are several key differences between the network structure outlined above and the traditional Blockchain
+        architecture regarding the creation of a distributed public ledger.
       </p>
       <p>
         An inherent defect in the modern Blockchain architecture is the inefficient composition of blocks. Blocks in the
         Bitcoin Blockchain are composed of multiple unrelated transactions. This indicates that within any given block,
         the earliest transactions would have experienced significant delays as later transactions continued to
-        accumulate until the entire block was verified. If one was only considering performance of the Blockchain and
-        nothing else, they would arrive at the conclusion that in an ideal architecture transactions would be validated
-        immediately as they were received rather than waiting to be bundled along with additional unrelated
+        accumulate until the entire block was verified. If one was only considering the performance of the Blockchain
+        and nothing else, they would arrive at the conclusion that in an ideal architecture transactions would be
+        validated immediately as they were received rather than waiting to be bundled along with additional unrelated
         transactions.
       </p>
       <p>
-        By removing the inclusion of unrelated transactions into blocks we are able to substantially reduce the average
-        network block size. This is accomplished through constructing blocks that group related transactions only.
-        Related transactions refer to a set of transactions that must be processed together. This is discussed in more
-        detail in the Blocks section of the paper.
+        By removing the inclusion of unrelated transactions into the blocks we are able to substantially reduce the
+        average network block size. This is accomplished through constructing blocks that group related transactions
+        only. Related transactions refer to a set of transactions that must be processed together. This is discussed in
+        more detail in the <NavLink to="/docs/blocks">Blocks</NavLink> section of the documentation.
       </p>
       <p>
-        These improvements, along with others discussed throughout this documentation, when implemented correctly reduce
-        the overall energy consumption and processing power required by the network. This increased efficiency not only
-        enables transactions between users to be verified within seconds (or less), but allows developers to utilize
-        more popular programming languages (Python, JavaScript, etc...) towards expediting early development while
-        encouraging rapid growth and innovation.
+        These improvements, along with others discussed throughout this documentation, when implemented correctly,
+        reduce the overall energy consumption and processing power required by the network. This increased efficiency
+        not only enables transactions between accounts to be verified within seconds (or less), but allows developers to
+        utilize more popular programming languages (Python, JavaScript, etc...) towards expediting early development
+        while encouraging rapid growth and innovation.
       </p>
     </section>
   );
