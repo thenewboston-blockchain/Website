b0799ca24336f1c6e226be8fc60518601a4403c6
Bucky <buckyroberts@gmail.com>
2020-07-01 18:38:37 -0700

    Updating docs

--- src/containers/BankAPI/BankRegistrations/code-snippets/client-to-bank-request.ts
+++ src/containers/BankAPI/BankRegistrations/code-snippets/client-to-bank-request.ts
@@ -13,13 +13,8 @@ export default `{
       },
       "signature": "a341eb2d678df410fb110760fd2c77c5969975d4fcba9a3846d9f11dfb43151bc23a157c26dd29163f061697806bc2b75d74a300ed6ba1a504ae2de6013d8c0f"
     },
-    "ip_address": "192.168.1.232",
-    "pk": "7e3d50d5-5d2e-44c3-bfe0-e14f97984197",
-    "port": 8000,
-    "protocol": "http",
-    "validator_node_identifier": "3afdf37573f1a511def0bd85553404b7091a76bcd79cdcebba1310527b167521",
-    "version": "v1.0"
+    "validator_node_identifier": "3afdf37573f1a511def0bd85553404b7091a76bcd79cdcebba1310527b167521"
   },
   "node_identifier": "d5356888dc9303e44ce52b1e06c3165a7759b9df1e6a6dfbd33ee1c3df1ab4d1",
-  "signature": "670a49afea0f9ee34b2c4a103c4be51773ed6b5fdf69dd1e4ededabf42347f962cfcccd5f66f5219b8800eb962d3269c0e531d26eebe2d0657083f620282cf0c"
+  "signature": "6cdc22862bf0ca889bcc07106fe37f92b23a5ff1a91449f19655dace32367af1d12cf7967acb1aab8f42a253e146ecbce2a4941fbe33a7553603766a9df7470f"
 }`;
--- src/containers/BankAPI/BankRegistrations/index.tsx
+++ src/containers/BankAPI/BankRegistrations/index.tsx
@@ -5,6 +5,8 @@ import RequestResponse from 'components/RequestResponse';
 
 import bankToValidatorRequest from './code-snippets/bank-to-validator-request';
 import bankToValidatorResponse from './code-snippets/bank-to-validator-response';
+import clientToBankRequest from './code-snippets/client-to-bank-request';
+import clientToBankResponse from './code-snippets/client-to-bank-response';
 import validatorToBankRequest from './code-snippets/validator-to-bank-request';
 import validatorToBankResponse from './code-snippets/validator-to-bank-response';
 
@@ -12,81 +14,120 @@ import BankRegistrationDiagram from './BankRegistrations.png';
 
 import './BankRegistrations.scss';
 
-const BankRegistrations = () => (
-  <div className="BankRegistrations">
-    <h1 className="page-title">Bank Registrations</h1>
-    <p>
-      Banks must register with validators before they are accepted onto the network by that validator. This is done in a
-      similar fashion to the registration process between user accounts and banks. To register, banks will pay a
-      registration fee to the validator.
-    </p>
-    <p>
-      Rather than sending the registration request directly from client to the validator, the request will be sent
-      through the bank. The bank will then forward it along to the validator. The purpose of routing the request through
-      the bank is so that the bank can first create a record of the registration.
-    </p>
+const BankRegistrations = () => {
+  const renderBankToValidator = () => (
+    <>
+      <h2 className="endpoint">POST /bank_registrations (Bank > Validator)</h2>
+      <ParamsTable
+        items={[
+          {
+            param: 'message',
+            dataType: 'object',
+            description: 'validator_node_identifier to register with and block payment for registration fee',
+          },
+          {
+            param: 'node_identifier',
+            dataType: 'string',
+            description: 'node identifier of the bank',
+          },
+          {
+            param: 'signature',
+            dataType: 'string',
+            description: 'hex value of the signed message',
+          },
+        ]}
+      />
+      <RequestResponse code={bankToValidatorRequest} heading="Request" />
+      <p>
+        After receiving the registration request, the validator will create a bank registration which will be initially
+        set to "pending". The validator then responds to the bank as confirmation that the request had been received.
+      </p>
+      <RequestResponse code={bankToValidatorResponse} heading="Response" />
+      <p>
+        The validator then performs a network background check of the applying bank. During this process, validators
+        will check the bank's trust level with other existing banks. Banks must also prove that they are configured
+        properly to act as a bank node. This is done through the ability to act as a server by responding properly to
+        network requests made from the validator to the bank's IP address. This verification prevents end users from
+        acting as banks by sending transactions directly to the validator.
+      </p>
+    </>
+  );
 
-    <div className="img-container">
-      <img alt="bank registration diagram" className="bank-registration-diagram" src={BankRegistrationDiagram} />
+  const renderClientToBank = () => (
+    <>
+      <h2 className="endpoint">POST /bank_registrations (Client > Bank)</h2>
+      <ParamsTable
+        items={[
+          {
+            param: 'message',
+            dataType: 'object',
+            description: 'validator_node_identifier to register with and block payment for registration fee',
+          },
+          {
+            param: 'node_identifier',
+            dataType: 'string',
+            description: 'node identifier of the bank',
+          },
+          {
+            param: 'signature',
+            dataType: 'string',
+            description: 'hex value of the signed message',
+          },
+        ]}
+      />
+      <RequestResponse code={clientToBankRequest} heading="Request" />
+      <RequestResponse code={clientToBankResponse} heading="Response" />
+    </>
+  );
+
+  const renderValidatorToBank = () => (
+    <>
+      <h2 className="endpoint">{'PATCH /bank_registrations/{id} (Validator > Bank)'}</h2>
+      <ParamsTable
+        items={[
+          {
+            param: 'message',
+            dataType: 'object',
+            description: 'status to indicate the result of the bank registration',
+          },
+          {
+            param: 'node_identifier',
+            dataType: 'string',
+            description: 'node identifier of the primary validator',
+          },
+          {
+            param: 'signature',
+            dataType: 'string',
+            description: 'hex value of the signed message',
+          },
+        ]}
+      />
+      <RequestResponse code={validatorToBankRequest} heading="Request" />
+      <RequestResponse code={validatorToBankResponse} heading="Response" />
+    </>
+  );
+
+  return (
+    <div className="BankRegistrations">
+      <h1 className="page-title">Bank Registrations</h1>
+      <p>
+        Banks must register with validators before they are accepted onto the network by that validator. This is done in
+        a similar fashion to the registration process between user accounts and banks. To register, banks will pay a
+        registration fee to the validator.
+      </p>
+      <p>
+        Rather than sending the registration request directly from client to the validator, the request will be sent
+        through the bank. The bank will then forward it along to the validator. The purpose of routing the request
+        through the bank is so that the bank can first create a record of the registration.
+      </p>
+      <div className="img-container">
+        <img alt="bank registration diagram" className="bank-registration-diagram" src={BankRegistrationDiagram} />
+      </div>
+      {renderClientToBank()}
+      {renderBankToValidator()}
+      {renderValidatorToBank()}
     </div>
-
-    <h2 className="endpoint">POST /bank_registrations</h2>
-    <ParamsTable
-      items={[
-        {
-          param: 'message',
-          dataType: 'object',
-          description: 'validator_node_identifier to register with and block payment for registration fee',
-        },
-        {
-          param: 'node_identifier',
-          dataType: 'string',
-          description: 'node identifier of the bank',
-        },
-        {
-          param: 'signature',
-          dataType: 'string',
-          description: 'hex value of the signed message',
-        },
-      ]}
-    />
-    <RequestResponse code={bankToValidatorRequest} heading="Request (Bank > Validator)" />
-    <p>
-      After receiving the registration request, the validator will create a bank registration which will be initially
-      set to "pending". The validator then responds to the bank as confirmation that the request had been received.
-    </p>
-    <RequestResponse code={bankToValidatorResponse} heading="Response (Validator > Bank)" />
-    <p>
-      The validator then performs a network background check of the applying bank. During this process, validators will
-      check the bank's trust level with other existing banks. Banks must also prove that they are configured properly to
-      act as a bank node. This is done through the ability to act as a server by responding properly to network requests
-      made from the validator to the bank's IP address. This verification prevents end users from acting as banks by
-      sending transactions directly to the validator.
-    </p>
-
-    <h2 className="endpoint">{'PATCH /bank_registrations/{id}'}</h2>
-    <ParamsTable
-      items={[
-        {
-          param: 'message',
-          dataType: 'object',
-          description: 'status to indicate the result of the bank registration',
-        },
-        {
-          param: 'node_identifier',
-          dataType: 'string',
-          description: 'node identifier of the primary validator',
-        },
-        {
-          param: 'signature',
-          dataType: 'string',
-          description: 'hex value of the signed message',
-        },
-      ]}
-    />
-    <RequestResponse code={validatorToBankRequest} heading="Request (Validator > Bank)" />
-    <RequestResponse code={validatorToBankResponse} heading="Response (Bank > Validator):" />
-  </div>
-);
+  );
+};
 
 export default BankRegistrations;
