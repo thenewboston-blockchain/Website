fc7f4e9c690354cfe6570251bf7e67224a1e2ba6
Bucky <buckyroberts@gmail.com>
2020-07-07 20:38:52 -0700

    Primary validator API stubs

--- src/containers/PrimaryValidatorAPI/index.tsx
+++ src/containers/PrimaryValidatorAPI/index.tsx
@@ -1,14 +1,36 @@
 import React, {useMemo} from 'react';
 import {useParams} from 'react-router-dom';
 
+import ConnectionRequests from 'containers/NodeAPI/ConnectionRequests';
+
+import Accounts from './Accounts';
+import BankBlocks from './BankBlocks';
+import Banks from './Banks';
+import Config from './Config';
+import ConfirmationBlocks from './ConfirmationBlocks';
 import Introduction from './Introduction';
+import Validators from './Validators';
 
 import './PrimaryValidatorAPI.scss';
 
 const getPageContent = (chapter: string) => {
   switch (chapter) {
+    case 'accounts':
+      return <Accounts />;
+    case 'bank-blocks':
+      return <BankBlocks />;
+    case 'banks':
+      return <Banks />;
+    case 'config':
+      return <Config />;
+    case 'confirmation-blocks':
+      return <ConfirmationBlocks />;
+    case 'connection-requests':
+      return <ConnectionRequests />;
     case 'introduction':
       return <Introduction />;
+    case 'validators':
+      return <Validators />;
     default:
       return <Introduction />;
   }
