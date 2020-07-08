0cb16bc1a135f9a4a37bcf85fe7926c76736ce9f
Bucky <buckyroberts@gmail.com>
2020-07-07 18:33:10 -0700

    Connection Requests

--- src/containers/BankAPI/ConnectionRequests/index.tsx
+++ src/containers/BankAPI/ConnectionRequests/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import RequestResponse from 'components/RequestResponse';
 
+import ConnectionRequestDiagram from './ConnectionRequest.png';
+
 import signedConnectionRequest from './code-snippets/signed-connection-request';
 
 import './ConnectionRequests.scss';
@@ -10,10 +12,25 @@ const ConnectionRequests = () => {
   return (
     <section className="ConnectionRequests">
       <h1 className="page-title">Connection Requests</h1>
-      <p>Hey</p>
+      <p>To connect with another node on the network, a signed connection request can be sent to the target node.</p>
+
+      <div className="img-container">
+        <img alt="connection request" className="connection-request" src={ConnectionRequestDiagram} />
+      </div>
 
       <h2 className="endpoint">POST /connection_requests</h2>
-      <RequestResponse code={signedConnectionRequest} heading="Response" />
+      <RequestResponse code={signedConnectionRequest} heading="Request" />
+
+      <p>
+        When the target node receives the request, they will attempt to query the config data of the requesting node. If
+        data is retrieved successfully and the response body matches the data provided in the initial request, then the
+        target node will create a Bank or Validator, depending on the requesting nodes node_type. Note that on
+        successful connection, no meaningful response data is sent back, only an empty object to conform to network
+        protocol. This is because the requesting node does not care how they are stored on the target node, only that
+        the connection exists.
+      </p>
+
+      <RequestResponse code={`{}`} heading="Response" />
     </section>
   );
 };
