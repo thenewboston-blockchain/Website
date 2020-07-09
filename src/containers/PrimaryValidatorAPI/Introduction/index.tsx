395ccce292efd4c822823c7f9c750356ca0ffacc
Bucky <buckyroberts@gmail.com>
2020-07-09 15:25:08 -0700

    Starting on PV

--- src/containers/PrimaryValidatorAPI/Introduction/index.tsx
+++ src/containers/PrimaryValidatorAPI/Introduction/index.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Introduction = () => (
   <>
     <h1 className="page-title">Introduction</h1>
-    <p>Hello. I am a primary validator.</p>
+    <p>
+      The purpose of the primary validator is to validate transactions. Upon validation of a block, the validator will
+      append that block onto their blockchain and also update the balances of all accounts involved. The primary
+      validator will then forward the confirmed block to all connected confirmation validators so that the resulting
+      balances can be verified.
+    </p>
   </>
 );
 
