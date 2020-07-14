d9358f38acb5e732821560a35645e601caa9e230
Jacob Clevenger <jacobrclevenger@gmail.com>
2020-07-13 23:31:04 -0400

    Fixed issue #39. Left nav is now responsive. Top navigation still needs work

--- src/containers/AdminLayout/index.tsx
+++ src/containers/AdminLayout/index.tsx
@@ -1,18 +1,41 @@
-import React, {FC, ReactNode} from 'react';
+import React, {FC, ReactNode, useState, ReactElement} from 'react';
 
 import './AdminLayout.scss';
 
 interface ComponentProps {
   left: ReactNode;
   right: ReactNode;
-  top: ReactNode;
+  top: ReactElement;
 }
 
+
+
 export const AdminLayout: FC<ComponentProps> = ({left, right, top}) => {
+
+  const [isLeftMenuOpen, setLeftMenuOpen] = useState(false);
+
+  const handleEscape = (e: any) => {
+    console.log(e);
+    if (e.key === 'Escape') {
+      close();
+    }
+  }
+
+  const open = () => {
+    window.addEventListener('keydown', handleEscape);
+    setLeftMenuOpen(true);
+  };
+
+  const close = () => {
+    window.removeEventListener('keydown', handleEscape);
+    setLeftMenuOpen(false);
+  };
+
   return (
     <div className="AdminLayout">
-      <div className="top">{top}</div>
-      <div className="left">{left}</div>
+      <div className={`scrim ${ isLeftMenuOpen ? '' : 'hidden'}`} onClick={() => close()}></div>
+      <div className="top">{React.cloneElement(top, { open })}</div>
+      <div className={`left ${ isLeftMenuOpen ? 'isOpen' : '' }`}>{left}</div>
       <div className="right">{right}</div>
     </div>
   );
--- src/containers/TopNav/index.tsx
+++ src/containers/TopNav/index.tsx
@@ -5,19 +5,33 @@ import Logo from 'assets/images/logo.png';
 
 import './TopNav.scss';
 
-const TopNav = () => {
+
+const TopNav = (props: any) => {
+
+
   return (
     <div className="TopNav">
+      {props.open ?
+      <button onClick={() => props.open(true)} className="TopNav_MenuButton">
+        <svg viewBox="0 0 24 24">
+          <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+        </svg>
+      </button>
+      : ''}
+
       <NavLink className="thenewboston" to="/">
         <img alt="thenewboston logo" className="logo" src={Logo} />
         <span>thenewboston</span>
       </NavLink>
-      <NavLink to="/docs">Documentation</NavLink>
-      <NavLink to="/contributors">Contributors</NavLink>
-      <NavLink to="/bank-api">Bank API</NavLink>
-      <NavLink to="/confirmation-validator-api">Confirmation Validator API</NavLink>
-      <NavLink to="/primary-validator-api">Primary Validator API</NavLink>
-      <NavLink to="/deployment-guides">Deployment Guides</NavLink>
+
+      <div className="TopNav__Links">
+        <NavLink to="/docs">Documentation</NavLink>
+        <NavLink to="/contributors">Contributors</NavLink>
+        <NavLink to="/bank-api">Bank API</NavLink>
+        <NavLink to="/confirmation-validator-api">Confirmation Validator API</NavLink>
+        <NavLink to="/primary-validator-api">Primary Validator API</NavLink>
+        <NavLink to="/deployment-guides">Deployment Guides</NavLink>
+      </div>
     </div>
   );
 };
