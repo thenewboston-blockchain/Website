3b3f9a1d375a736b0af6825589971cd0c7d22666
Bucky <buckyroberts@gmail.com>
2020-07-14 20:28:14 -0700

    Remove pre-release commands

--- src/containers/DeploymentGuides/ProjectSetup/index.tsx
+++ src/containers/DeploymentGuides/ProjectSetup/index.tsx
@@ -7,14 +7,6 @@ const ProjectSetup = () => {
     <>
       <h2>Project Setup</h2>
 
-      <Commands code={`ssh-keygen -t rsa -b 4096 -C "bucky@hotmale.com"`} comment="Generate SSH key pair" />
-      <Commands code={`cat ~/.ssh/id_rsa.pub`} comment="Display SSH key in console" />
-
-      <p>
-        Add your SSH key to GitHub: <a href="https://github.com/settings/keys">https://github.com/settings/keys</a>
-      </p>
-
-      <Commands code={`ssh -T git@github.com`} comment="Test connection" />
       <Commands code={`sudo chmod go+w /var/www`} comment="Update /var/www/ permissions" />
       <Commands
         code={`cd
@@ -22,20 +14,6 @@ git clone git@github.com:thenewboston-developers/Validator.git /var/www/Validato
 cd /var/www/Validator/
 `}
         comment="Clone project to server"
-      />
-      <Commands
-        code={`nano requirements/local.txt
-# Remove first requirement (private GitHub repo)
-sudo pip3 install -r requirements/local.txt`}
-        comment="Temp 1/2"
-      />
-      <Commands
-        code={`cd /var/www/
-git clone git@github.com:thenewboston-developers/thenewboston-python.git
-cd /var/www/Validator/
-sudo pip3 install file:///var/www/thenewboston-python/dist/thenewboston-0.0.1.tar.gz
-`}
-        comment="Temp 2/2"
       />
     </>
   );
