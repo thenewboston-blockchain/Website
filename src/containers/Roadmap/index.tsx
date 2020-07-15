be2208e12fcd7796801dd800c6da080fe8ed0009
Bucky <buckyroberts@gmail.com>
2020-07-15 16:43:50 -0700

    Roadmap preview

--- src/containers/Roadmap/index.tsx
+++ src/containers/Roadmap/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import RoadmapUI from './RoadmapUI.png';
+
 import './Roadmap.scss';
 
 const Roadmap = () => {
   return (
     <div className="Roadmap">
       <h2>Coming soon!</h2>
+      <div className="img-container">
+        <img alt="roadmap" className="roadmap" src={RoadmapUI} />
+      </div>
     </div>
   );
 };
