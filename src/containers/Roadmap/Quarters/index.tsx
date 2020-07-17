4f8b56862f4c140c6bd7f0c6fd50707f1c6598eb
Justin Kim <justinkim943@gmail.com>
2020-07-16 18:08:26 -0700

    index is unnecessary

--- src/containers/Roadmap/Quarters/index.tsx
+++ src/containers/Roadmap/Quarters/index.tsx
@@ -4,8 +4,8 @@ import './Quarters.scss';
 
 const Quarters = () => {
   const renderQuarter = useMemo(
-    () => (quarter: string, index: number) => (
-      <div key={index} className={`flex-1 quarter ${quarter.toLowerCase()}`}>
+    () => (quarter: string) => (
+      <div key={quarter} className={`flex-1 quarter ${quarter.toLowerCase()}`}>
         <div className="arrow" />
         <div className="quarter-circle">
           <span className="quarter-name">{quarter}</span>
@@ -20,7 +20,7 @@ const Quarters = () => {
       <div className="flex-1" />
       <div className="flex-row quarters-row">
         <div className="flex-1" />
-        {['Q1', 'Q2', 'Q3', 'Q4'].map((quarter, index) => renderQuarter(quarter, index))}
+        {['Q1', 'Q2', 'Q3', 'Q4'].map((quarter) => renderQuarter(quarter))}
       </div>
     </div>
   );
