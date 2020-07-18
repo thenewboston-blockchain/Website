6420a1a56a522154fbbe53618b88acb8a2c94789
bucky <buckyroberts@gmail.com>
2020-07-18 09:33:36 -0700

    Cleaning

--- src/containers/PrimaryValidatorAPI/BankBlocks/index.tsx
+++ src/containers/PrimaryValidatorAPI/BankBlocks/index.tsx
@@ -9,9 +9,9 @@ const BankBlocks = () => {
     <section className="BankBlocks">
       <h1 className="page-title">Bank Blocks</h1>
       <p>
-        Bank blocks are blocks that have been signed by banks to indicate that the block has passed initial validation.
-        Blocks will be sent from end users to banks, and upon initial validation of those blocks the bank will sign the
-        block and forward it to the primary validator for final validation.
+        Bank blocks are blocks that have been signed by banks as a way of indicating that the block has passed initial
+        validation. Blocks will be sent from end users to banks, and upon initial validation of those blocks the bank
+        will sign the block and forward it to the primary validator for final validation.
       </p>
 
       <h2 className="endpoint">POST /bank_blocks</h2>
