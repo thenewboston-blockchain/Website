19226b6db0eb575738df5c6005e5935f4af5d4f1
bucky <buckyroberts@gmail.com>
2020-07-18 09:58:53 -0700

    More sample data

--- src/containers/PrimaryValidatorAPI/ConfirmationBlocks/index.tsx
+++ src/containers/PrimaryValidatorAPI/ConfirmationBlocks/index.tsx
@@ -7,7 +7,11 @@ import './ConfirmationBlocks.scss';
 const ConfirmationBlocks = () => (
   <div className="ConfirmationBlocks">
     <h1 className="page-title">Confirmation Blocks</h1>
-    <p>Hey</p>
+    <p>
+      Often referred to as a "confirmation", a block that has been signed by a validator as confirmation it has been
+      added to their blockchain, indicating that the transactions have been validated and that the points have been
+      successfully transferred
+    </p>
 
     <h2 className="endpoint">GET /sample</h2>
     <RequestResponse code={`{}`} heading="Response" />
