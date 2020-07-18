350e578864a58e78496481eb970335a738704458
bucky <buckyroberts@gmail.com>
2020-07-18 10:50:15 -0700

    Adding more sample API data

--- src/containers/ConfirmationValidatorAPI/Banks/index.tsx
+++ src/containers/ConfirmationValidatorAPI/Banks/index.tsx
@@ -8,10 +8,25 @@ const Banks = () => {
   return (
     <section className="Banks">
       <h1 className="page-title">Banks</h1>
-      <p>Hey</p>
+      <p>The confirmation validators will maintain a record of all connected banks.</p>
 
-      <h2 className="endpoint">GET /sample</h2>
-      <RequestResponse code={`{}`} heading="Response" />
+      <h2 className="endpoint">GET /banks</h2>
+      <RequestResponse
+        code={`[
+  {
+    "account_number": "5e12967707909e62b2bb2036c209085a784fabbc3deccefee70052b6181c8ed8",
+    "ip_address": "192.168.1.232",
+    "node_identifier": "d5356888dc9303e44ce52b1e06c3165a7759b9df1e6a6dfbd33ee1c3df1ab4d1",
+    "port": 8000,
+    "protocol": "http",
+    "version": "v1.0",
+    "default_transaction_fee": "1.0000000000000000",
+    "confirmation_expiration": null,
+    "trust": "0.00"
+  }
+]`}
+        heading="Response"
+      />
     </section>
   );
 };
