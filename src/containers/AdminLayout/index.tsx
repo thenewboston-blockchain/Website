a8aae17e41d2c592eb9674039e888f56dc378fad
Justin Kim <justinkim943@gmail.com>
2020-07-21 08:54:16 -0700

    Pull/88 Issue #79 - Fix Scroll on Route Change (#91)
    * fix scroll on route change
    
    * create reusable component for scroll fix
    
    * use reusable component to handle scroll issues
    
    * simplify the scrolling
    
    Co-authored-by: whitehox <oketegah@gmail.com>

--- src/containers/AdminLayout/index.tsx
+++ src/containers/AdminLayout/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, ReactNode, useEffect, useState} from 'react';
+import React, {FC, ReactNode, useEffect, useRef, useState} from 'react';
 import {NavLink, useLocation} from 'react-router-dom';
 
 import Logo from 'assets/images/logo.png';
@@ -12,11 +12,16 @@ interface ComponentProps {
 
 export const AdminLayout: FC<ComponentProps> = ({left, right}) => {
   const [leftMenuOpen, toggleLeftMenuOpen] = useState(window.innerWidth > 1200);
-  const location = useLocation();
+  const {pathname} = useLocation();
+  const rightDiv = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     toggleLeftMenuOpen(window.innerWidth > 1200);
-  }, [location]);
+  }, []);
+
+  useEffect(() => {
+    rightDiv.current?.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <div className={`AdminLayout ${leftMenuOpen ? 'left-menu-open' : ''}`}>
@@ -32,7 +37,9 @@ export const AdminLayout: FC<ComponentProps> = ({left, right}) => {
         </div>
       </div>
       <div className="left">{left}</div>
-      <div className="right">{right}</div>
+      <div className="right" ref={rightDiv}>
+        {right}
+      </div>
     </div>
   );
 };
