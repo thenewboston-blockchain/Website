fd40c4abfde829f04909c39a43f2f8873641f043
Justin Kim <justinkim943@gmail.com>
2020-07-26 15:02:07 -0700

    Did Contribute page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import {Commands, RequestResponse} from './CodeHighlighter';
 import {DocContainer, DocEndpoint, DocList, DocImage, DocSubSection} from './DocWrapper';
+import {Button} from './FormElements';
 import MarketingButton from './MarketingButton';
 import {TableBorderGrid, TableParams, TableVertical} from './Table';
 
 export {
+  Button,
   Commands,
   DocContainer,
   DocEndpoint,
--- src/containers/Contribute/index.tsx
+++ src/containers/Contribute/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, {FC, ReactNode} from 'react';
 import {NavLink} from 'react-router-dom';
 
-import {MarketingButton} from 'components';
+import {Button, MarketingButton} from 'components';
 
 import Arm from './Arm.png';
-
 import './Contribute.scss';
 
-const Contribute = () => {
-  const renderDesign = () => (
-    <div className="navigation-column">
-      <h2 className="development-heading">Design</h2>
+const Contribute: FC = () => {
+  const renderDesign = (): ReactNode => (
+    <div className="Contribute__navigation-column">
+      <h2 className="Contribute__navigation-column-header">Design</h2>
 
-      <div className="navigation-group">
-        <h2>Resources</h2>
+      <div className="Contribute__navigation-group">
+        <h2 className="Contribute__navigation-group-header">Resources</h2>
         <a
+          className="Contribute__a"
           href="https://www.figma.com/file/ZLQBaMEsAQIdnfg5qtL384/TNB-System?node-id=12%3A113"
           target="_blank"
           rel="noopener noreferrer"
@@ -22,6 +22,7 @@ const Contribute = () => {
           Style Guide
         </a>
         <a
+          className="Contribute__a"
           href="https://docs.google.com/document/d/1gRy71vQrHGDk2bZ4Wcz3ha4xHjNmOtcXCkDXtLeqt-4/edit"
           target="_blank"
           rel="noopener noreferrer"
@@ -32,43 +33,75 @@ const Contribute = () => {
     </div>
   );
 
-  const renderPythonDjango = () => (
-    <div className="navigation-column">
-      <h2 className="development-heading">Python / Django</h2>
+  const renderPythonDjango = (): ReactNode => (
+    <div className="Contribute__navigation-column">
+      <h2 className="Contribute__navigation-column-header">Python / Django</h2>
 
-      <div className="navigation-group">
-        <h2>Bank</h2>
-        <NavLink to="/bank-api/accounts">Bank API Docs</NavLink>
-        <a href="https://github.com/thenewboston-developers/Bank" target="_blank" rel="noopener noreferrer">
+      <div className="Contribute__navigation-group">
+        <h2 className="Contribute__navigation-group-header">Bank</h2>
+        <NavLink className="Contribute__a" to="/bank-api/accounts">
+          Bank API Docs
+        </NavLink>
+        <a
+          className="Contribute__a"
+          href="https://github.com/thenewboston-developers/Bank"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Repository
         </a>
-        <a href="https://github.com/thenewboston-developers/Bank/issues" target="_blank" rel="noopener noreferrer">
+        <a
+          className="Contribute__a"
+          href="https://github.com/thenewboston-developers/Bank/issues"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Issues / Tasks
         </a>
       </div>
 
-      <h2>Validator</h2>
-      <NavLink to="/confirmation-validator-api/accounts">Confirmation Validator API Docs</NavLink>
-      <NavLink to="/primary-validator-api/accounts">Primary Validator API Docs</NavLink>
-      <a href="https://github.com/thenewboston-developers/Validator" target="_blank" rel="noopener noreferrer">
+      <h2 className="Contribute__navigation-group-header">Validator</h2>
+      <NavLink className="Contribute__a" to="/confirmation-validator-api/accounts">
+        Confirmation Validator API Docs
+      </NavLink>
+      <NavLink className="Contribute__a" to="/primary-validator-api/accounts">
+        Primary Validator API Docs
+      </NavLink>
+      <a
+        className="Contribute__a"
+        href="https://github.com/thenewboston-developers/Validator"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Repository
       </a>
-      <a href="https://github.com/thenewboston-developers/Validator/issues" target="_blank" rel="noopener noreferrer">
+      <a
+        className="Contribute__a"
+        href="https://github.com/thenewboston-developers/Validator/issues"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Issues / Tasks
       </a>
     </div>
   );
 
   const renderReactFE = () => (
-    <div className="navigation-column">
-      <h2 className="development-heading">React / FE</h2>
+    <div className="Contribute__navigation-column">
+      <h2 className="Contribute__navigation-column-header">React / FE</h2>
 
-      <div className="navigation-group">
-        <h2>Account Manager</h2>
-        <a href="https://github.com/thenewboston-developers/Account-Manager" target="_blank" rel="noopener noreferrer">
+      <div className="Contribute__navigation-group">
+        <h2 className="Contribute__navigation-group-header">Account Manager</h2>
+        <a
+          className="Contribute__a"
+          href="https://github.com/thenewboston-developers/Account-Manager"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Repository
         </a>
         <a
+          className="Contribute__a"
           href="https://github.com/thenewboston-developers/Account-Manager/issues"
           target="_blank"
           rel="noopener noreferrer"
@@ -77,33 +110,41 @@ const Contribute = () => {
         </a>
       </div>
 
-      <h2>Website</h2>
-      <a href="https://github.com/thenewboston-developers/Website" target="_blank" rel="noopener noreferrer">
+      <h2 className="Contribute__navigation-group-header">Website</h2>
+      <a
+        className="Contribute__a"
+        href="https://github.com/thenewboston-developers/Website"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Repository
       </a>
-      <a href="https://github.com/thenewboston-developers/Website/issues" target="_blank" rel="noopener noreferrer">
+      <a
+        className="Contribute__a"
+        href="https://github.com/thenewboston-developers/Website/issues"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Issues / Tasks
       </a>
     </div>
   );
 
-  const renderStepIndicator = (color: string, number: number, text: string) => (
-    <div className="step-indicator">
-      <div>
-        <div className="step-bubble" style={{backgroundColor: color}}>
-          {number}
-        </div>
+  const renderStepIndicator = (color: string, number: number, text: string): ReactNode => (
+    <div className="Contribute__step-indicator">
+      <div className="Contribute__step-bubble" style={{backgroundColor: color}}>
+        {number}
       </div>
-      <div className="step-text">{text}</div>
+      <div className="Contribute__step-text">{text}</div>
     </div>
   );
 
-  const renderStepOne = () => (
-    <div className="step-one">
+  const renderStepOne = (): ReactNode => (
+    <div className="Contribute__step-one">
       {renderStepIndicator('#f4c2c4', 1, 'Get started on thenewboston')}
-      <div className="button-container">
+      <div className="Contribute__button-container">
         <NavLink to="/guide/introduction">
-          <button className="primary">Documentation</button>
+          <Button className="Contribute__doc-button">Documentation</Button>
         </NavLink>
         <MarketingButton website="slack" />
         <MarketingButton website="github" />
@@ -112,10 +153,10 @@ const Contribute = () => {
     </div>
   );
 
-  const renderStepTwo = () => (
-    <div className="step-two">
+  const renderStepTwo = (): ReactNode => (
+    <div className="Contribute__step-two">
       {renderStepIndicator('#b7d6ff', 2, 'Choose your weapon')}
-      <div className="navigation-section">
+      <div className="Contribute__navigation-section">
         {renderPythonDjango()}
         {renderReactFE()}
         {renderDesign()}
@@ -127,7 +168,7 @@ const Contribute = () => {
     <div className="Contribute">
       {renderStepOne()}
       {renderStepTwo()}
-      <img alt="arm" className="arm" src={Arm} />
+      <img alt="arm" className="Contribute__arm" src={Arm} />
     </div>
   );
 };
