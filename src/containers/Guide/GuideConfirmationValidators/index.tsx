d0d4af2f8ae2238af296e08dbe8d26179e0062f4
Bucky <buckyroberts@gmail.com>
2020-08-02 17:34:48 -0700

    Confirmation validator page updates

--- src/containers/Guide/GuideConfirmationValidators/index.tsx
+++ src/containers/Guide/GuideConfirmationValidators/index.tsx
@@ -20,26 +20,26 @@ const GuideConfirmationValidators: FC = () => {
       <DocList variant="ul">
         <li>Root account file</li>
         <ul>
-          <li>this is the flattened representation of all account balances at a given point in time</li>
+          <li>This is the flattened representation of all account balances at a given point in time</li>
         </ul>
         <li>Root account file hash</li>
         <ul>
           <li>
-            after downloading the root account file from the primary validator, confirmation validators will hash their
+            After downloading the root account file from the primary validator, confirmation validators will hash their
             downloaded copy and compare it to the root account file hash provided by the primary validator to ensure the
             values match
           </li>
-          <li>this process is used to ensure that data integrity is maintained during the download process</li>
+          <li>This process is used to ensure that data integrity is maintained during the download process</li>
         </ul>
         <li>Seed block identifier</li>
         <ul>
-          <li>identifier of the last block that was used when the root account file was generated</li>
-          <li>used to verify all account balances on the root account file at that moment in time</li>
+          <li>Identifier of the last block that was used when the root account file was generated</li>
+          <li>Used to verify all account balances on the root account file at that moment in time</li>
         </ul>
         <li>Head block hash</li>
         <ul>
-          <li>value representing the message hash of the primary validators most recently verified block</li>
-          <li>used by the confirmation validators to know when syncing is complete</li>
+          <li>Value representing the message hash of the primary validators most recently verified block</li>
+          <li>Used by the confirmation validators to know when syncing is complete</li>
         </ul>
       </DocList>
 
@@ -48,14 +48,14 @@ const GuideConfirmationValidators: FC = () => {
       <p>When confirmation validators first come online, they will:</p>
 
       <DocList variant="ol">
-        <li>Download the root account file from the primary validator</li>
+        <li>Download the root account file from the primary validator.</li>
         <li>
           Hash the downloaded root account file and compare it to the hash provided by the primary validator to ensure
-          they match
+          they match.
         </li>
         <li>
           Store the block hash of the primary validators last validated block, also known as the HEAD block hash, for
-          reference (we will see how this is used later)
+          reference (we will see how this is used later).
         </li>
       </DocList>
 
@@ -70,13 +70,13 @@ const GuideConfirmationValidators: FC = () => {
         <li>Verify the block against their own account data</li>
         <ol type="a">
           <li>
-            if the confirmation validators results match the updated balances given by the primary validator (the block
-            is verified) the confirmation validator will:
+            If the confirmation validators results match the updated balances given by the primary validator (the block
+            is verified), the confirmation validator will:
           </li>
           <ol type="i">
-            <li>update the account balances</li>
+            <li>Update the account balances</li>
             <li>
-              generate a hash of the block message, which will become the block identifier of the next confirmed block,
+              Generate a hash of the block message, which will become the block identifier of the next confirmed block
               and repeat
             </li>
           </ol>
@@ -93,30 +93,30 @@ const GuideConfirmationValidators: FC = () => {
         <li>Obtain the seed block identifier from the new primary validator</li>
         <ol type="a">
           <li>
-            if the confirmation validator does not have this block (meaning the new validator was ahead in processing
-            blocks) then the confirmation validator must query the new primary validator with the address of the most
-            recent validated block and continue syncing until the seed block is found
+            If the confirmation validator does not have this block (meaning the new validator was ahead in processing
+            blocks), then the confirmation validator must query the new primary validator with the address of the most
+            recent validated block and continue syncing until the seed block is found.
           </li>
         </ol>
         <li>
           Beginning with the previous primary validators root account file, apply the confirmed blocks (already verified
-          by the confirmation validator) until reaching the seed block from the new primary validator
+          by the confirmation validator) until reaching the seed block from the new primary validator.
         </li>
         <li>Compare the updated account file to the root account file of the new primary validator</li>
         <ol type="a">
           <li>
-            if they match, accept them as the new primary validator and copy their root account file, hash of that base
-            account file, and hash of the base block over to the confirmation validators config
+            If they match, accept them as the new primary validator and copy their root account file, hash of that base
+            account file, and hash of the base block over to the confirmation validators config.
           </li>
         </ol>
       </DocList>
 
       <p>
-        When a new block is verified by the primary validator, the primary validator will send a POST request to all
-        confirmation validators. This will inform the confirmation validators of the updated data immediately, and
-        prevent the confirmation validators from needing to continuously make GET requests to the primary validator to
-        check for new blocks. The primary validator has incentive to reliably forward all confirmation blocks to the
-        confirmation validators since this will help the primary validators trust level remain high among all banks.
+        When the primary validator verifies a new block, it will send a POST request to all confirmation validators.
+        This will inform the confirmation validators of the updated data immediately, and prevent the confirmation
+        validators from needing to continuously make GET requests to the primary validator to check for new blocks. The
+        primary validator has incentive to reliably forward all confirmation blocks to the confirmation validators,
+        since this will help the primary validator's trust level remain high among all banks.
       </p>
       <p>
         Given that confirmation validators do not earn points through transaction fees like the primary validator, the
