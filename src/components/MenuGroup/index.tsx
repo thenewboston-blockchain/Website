1e30ad741a90534d87fe15ed83da72f29557014d
Justin Kim <justinkim943@gmail.com>
2020-08-06 18:51:47 -0700

    finished the components

--- src/components/MenuGroup/index.tsx
+++ src/components/MenuGroup/index.tsx
@@ -1,5 +1,6 @@
 import React, {FC, ReactNode, useState} from 'react';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
+import clsx from 'clsx';
 
 import {Icon, IconType} from 'components';
 import {getFirstPathParam} from 'utils/urls';
@@ -18,15 +19,15 @@ const MenuGroup: FC<ComponentProps> = ({children, location, title, urlBase}) => 
   return (
     <div className="MenuGroup">
       <div
-        className={`MenuGroup__toggle ${expanded ? 'MenuGroup__toggle--expanded' : ''}`}
+        className={clsx('MenuGroup__toggle', {'MenuGroup__toggle--expanded': expanded})}
         onClick={() => toggleExpanded(!expanded)}
         role="button"
         tabIndex={0}
       >
-        <Icon className="react-icons MenuGroup__toggle-arrow" icon={IconType.menuRight} size={24} />
-        <span className="heavy">{title}</span>
+        <Icon className="MenuGroup__toggle-arrow" icon={IconType.menuRight} size={24} />
+        <span className="MenuGroup__title">{title}</span>
       </div>
-      <div className={`MenuGroup__submenu ${expanded ? 'MenuGroup__submenu--expanded' : ''}`}>{children}</div>
+      <div className={clsx('MenuGroup__submenu', {'MenuGroup__submenu--expanded': expanded})}>{children}</div>
     </div>
   );
 };
