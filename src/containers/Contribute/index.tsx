54b8a3f8197cebb685792d6a98393cd0a3f86993
Bucky Roberts <buckyroberts@gmail.com>
2020-08-08 14:38:04 -0700

    Bucky website updates (#134)
    * Remove "Designer" section and put resources under React/FE
    
    * Contributors page finsihed
    
    * Breadcrumb bar tweaks

--- src/containers/Contribute/index.tsx
+++ src/containers/Contribute/index.tsx
@@ -7,9 +7,64 @@ import Arm from './Arm.png';
 import './Contribute.scss';
 
 const Contribute: FC = () => {
-  const renderDesign = (): ReactNode => (
+  const renderPythonDjango = (): ReactNode => (
     <div className="Contribute__navigation-column">
-      <h2 className="Contribute__navigation-column-header">Design</h2>
+      <h2 className="Contribute__navigation-column-header">Python / Django</h2>
+
+      <div className="Contribute__navigation-group">
+        <h2 className="Contribute__navigation-group-header">Bank</h2>
+        <NavLink className="Contribute__a" to="/bank-api/accounts">
+          Bank API Docs
+        </NavLink>
+        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Bank">
+          Repository
+        </A>
+        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Bank/issues">
+          Issues / Tasks
+        </A>
+      </div>
+
+      <div className="Contribute__navigation-group">
+        <h2 className="Contribute__navigation-group-header">Validator</h2>
+        <NavLink className="Contribute__a" to="/confirmation-validator-api/accounts">
+          Confirmation Validator API Docs
+        </NavLink>
+        <NavLink className="Contribute__a" to="/primary-validator-api/accounts">
+          Primary Validator API Docs
+        </NavLink>
+        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Validator">
+          Repository
+        </A>
+        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Validator/issues">
+          Issues / Tasks
+        </A>
+      </div>
+    </div>
+  );
+
+  const renderReactFE = (): ReactNode => (
+    <div className="Contribute__navigation-column">
+      <h2 className="Contribute__navigation-column-header">React / FE</h2>
+
+      <div className="Contribute__navigation-group">
+        <h2 className="Contribute__navigation-group-header">Account Manager</h2>
+        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Account-Manager">
+          Repository
+        </A>
+        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Account-Manager/issues">
+          Issues / Tasks
+        </A>
+      </div>
+
+      <div className="Contribute__navigation-group">
+        <h2 className="Contribute__navigation-group-header">Website</h2>
+        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Website">
+          Repository
+        </A>
+        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Website/issues">
+          Issues / Tasks
+        </A>
+      </div>
 
       <div className="Contribute__navigation-group">
         <h2 className="Contribute__navigation-group-header">Resources</h2>
@@ -29,93 +84,33 @@ const Contribute: FC = () => {
     </div>
   );
 
-  const renderPythonDjango = (): ReactNode => (
-    <div className="Contribute__navigation-column">
-      <h2 className="Contribute__navigation-column-header">Python / Django</h2>
-
-      <div className="Contribute__navigation-group">
-        <h2 className="Contribute__navigation-group-header">Bank</h2>
-        <NavLink className="Contribute__a" to="/bank-api/accounts">
-          Bank API Docs
-        </NavLink>
-        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Bank">
-          Repository
-        </A>
-        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Bank/issues">
-          Issues / Tasks
-        </A>
-      </div>
-
-      <h2 className="Contribute__navigation-group-header">Validator</h2>
-      <NavLink className="Contribute__a" to="/confirmation-validator-api/accounts">
-        Confirmation Validator API Docs
-      </NavLink>
-      <NavLink className="Contribute__a" to="/primary-validator-api/accounts">
-        Primary Validator API Docs
-      </NavLink>
-      <A className="Contribute__a" href="https://github.com/thenewboston-developers/Validator">
-        Repository
-      </A>
-      <A className="Contribute__a" href="https://github.com/thenewboston-developers/Validator/issues">
-        Issues / Tasks
-      </A>
-    </div>
-  );
-
-  const renderReactFE = (): ReactNode => (
-    <div className="Contribute__navigation-column">
-      <h2 className="Contribute__navigation-column-header">React / FE</h2>
-
-      <div className="Contribute__navigation-group">
-        <h2 className="Contribute__navigation-group-header">Account Manager</h2>
-        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Account-Manager">
-          Repository
-        </A>
-        <A className="Contribute__a" href="https://github.com/thenewboston-developers/Account-Manager/issues">
-          Issues / Tasks
-        </A>
-      </div>
-
-      <h2 className="Contribute__navigation-group-header">Website</h2>
-      <A className="Contribute__a" href="https://github.com/thenewboston-developers/Website">
-        Repository
-      </A>
-      <A className="Contribute__a" href="https://github.com/thenewboston-developers/Website/issues">
-        Issues / Tasks
-      </A>
-    </div>
-  );
-
-  const renderStepIndicator = (color: string, number: number, text: string): ReactNode => (
+  const renderStepIndicator = (number: number, text: string): ReactNode => (
     <div className="Contribute__step-indicator">
-      <div className="Contribute__step-bubble" style={{backgroundColor: color}}>
-        {number}
-      </div>
+      <div className="Contribute__step-bubble">{number}</div>
       <div className="Contribute__step-text">{text}</div>
     </div>
   );
 
   const renderStepOne = (): ReactNode => (
     <div className="Contribute__step-one">
-      {renderStepIndicator('#f4c2c4', 1, 'Get started on thenewboston')}
+      {renderStepIndicator(1, 'Get started on thenewboston')}
       <div className="Contribute__button-container">
-        <NavLink to="/guide/introduction">
-          <Button className="Contribute__doc-button">Documentation</Button>
-        </NavLink>
         <MarketingButton website="slack" />
         <MarketingButton website="github" />
         <MarketingButton website="reddit" />
+        <NavLink to="/guide/introduction">
+          <Button className="Contribute__doc-button">View Guide</Button>
+        </NavLink>
       </div>
     </div>
   );
 
   const renderStepTwo = (): ReactNode => (
     <div className="Contribute__step-two">
-      {renderStepIndicator('#b7d6ff', 2, 'Choose your weapon')}
+      {renderStepIndicator(2, 'Choose your weapon')}
       <div className="Contribute__navigation-section">
         {renderPythonDjango()}
         {renderReactFE()}
-        {renderDesign()}
       </div>
     </div>
   );
