c53da7e3cea0cb1e8a30e84ba1bdff8a520539cd
Bucky <buckyroberts@gmail.com>
2020-09-16 19:44:22 -0700

    Normal case

--- src/containers/Home/HomeSteps/index.tsx
+++ src/containers/Home/HomeSteps/index.tsx
@@ -17,12 +17,14 @@ const HomeSteps = () => {
         </div>
         <div className="HomeSteps__Step">
           <StepIndicator number={2} text="Complete tasks" />
-          <div className="HomeSteps__instructional-text">The more difficult the task, the bigger the reward.</div>
+          <div className="HomeSteps__instructional-text">
+            The more difficult the <NavLink to="/guide/blocks">task</NavLink>, the bigger the reward.
+          </div>
         </div>
         <div className="HomeSteps__Step">
           <StepIndicator number={3} text="Earn points" />
           <div className="HomeSteps__instructional-text">
-            Climb the <NavLink to="/guide/blocks">Leaderboard</NavLink> to become a top developer.
+            Climb the <NavLink to="/guide/blocks">leaderboard</NavLink> to become a top developer.
           </div>
         </div>
       </div>
