50a8ad079f803eceaaa2721d5483a76309a8ef4f
Bucky <buckyroberts@gmail.com>
2020-09-18 14:20:12 -0700

    Cleaning

--- src/components/ContributorTasks/index.tsx
+++ src/components/ContributorTasks/index.tsx
@@ -17,36 +17,55 @@ const ContributorTasks: FC<ComponentProps> = ({className}) => {
           <span className="ContributorTasks__table-header">Amount</span>
         </div>
       ),
+      dateCompleted: <span className="ContributorTasks__table-header">Completed</span>,
+      repository: <span className="ContributorTasks__table-header">Repository</span>,
       title: <span className="ContributorTasks__table-header">Recent Task</span>,
     };
 
     const taskRows = [
       {
         amount: 25000,
+        dateCompleted: '6/19/2020',
+        repository: 'Account-Manager',
         title: 'Tab responsiveness',
       },
       {
         amount: 50000,
+        dateCompleted: '6/19/2020',
+        repository: 'Validator',
         title: 'When points are sent to friend the from sender account balance should get reduced',
       },
       {
         amount: 500,
+        dateCompleted: '6/19/2020',
+        repository: 'Account-Manager',
         title: "Create Jest Tests for 'formatPath' and 'formatPathFromNode'",
       },
       {
         amount: 30000,
+        dateCompleted: '6/19/2020',
+        repository: 'Bank',
         title: 'Import and export entire account manager profile',
       },
       {
         amount: 20000,
+        dateCompleted: '6/19/2020',
+        repository: 'Account-Manager',
         title: 'Import/export signing keys',
       },
-    ].map(({amount, title}) => ({
+    ].map(({amount, dateCompleted, repository, title}) => ({
       amount: <div className="ContributorTasks__amount">+ {amount.toLocaleString()}</div>,
+      dateCompleted: <span className="ContributorTasks__date-completed">{dateCompleted}</span>,
+      repository: <span className="ContributorTasks__repository">{repository}</span>,
       title: <span className="ContributorTasks__task-title">{title}</span>,
     }));
 
-    return [headerRow, ...taskRows].map(({amount, title}) => [title, amount]);
+    return [headerRow, ...taskRows].map(({amount, dateCompleted, repository, title}) => [
+      title,
+      repository,
+      dateCompleted,
+      amount,
+    ]);
   };
 
   return <TableVertical altColors className={clsx('ContributorTasks', className)} rows={getRecentTaskRows()} />;
