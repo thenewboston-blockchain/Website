49eb8dbef77fef2779ebb8b9f348bdc2159bce2d
Bucky Roberts <buckyroberts@gmail.com>
2020-10-02 19:38:58 -0700

    Get latest release from GitHub (#269)

--- src/containers/Download/index.tsx
+++ src/containers/Download/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
+import max from 'lodash/max';
 
 import {Button} from 'components';
+import {Release} from 'types/github';
+import {fetchGithubReleases} from 'utils/github';
 
 import './Download.scss';
 
 const Download = () => {
+  const [error, setError] = useState<boolean>(false);
+  const [releases, setReleases] = useState<Release[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const results = await fetchGithubReleases();
+        setReleases(results);
+      } catch (err) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const latestReleaseNumber = useMemo(() => {
+    const releaseNumbers = releases.map(({alphaVersion}) => alphaVersion);
+    return max(releaseNumbers);
+  }, [releases]);
+
   const renderOS = (name: string, link: string) => {
     return (
       <div className="Download__os">
@@ -16,19 +42,22 @@ const Download = () => {
     );
   };
 
+  if (error) return <h1>Error</h1>;
+  if (loading) return null;
+
   return (
     <div className="Download">
       {renderOS(
         'Windows',
-        'https://github.com/thenewboston-developers/Account-Manager/releases/download/v1.0.0-alpha.19/TNB-Account-Manager-1.0.0-alpha.19-win.exe',
+        `https://github.com/thenewboston-developers/Account-Manager/releases/download/v1.0.0-alpha.${latestReleaseNumber}/TNB-Account-Manager-1.0.0-alpha.${latestReleaseNumber}-win.exe`,
       )}
       {renderOS(
         'Mac',
-        'https://github.com/thenewboston-developers/Account-Manager/releases/download/v1.0.0-alpha.19/TNB-Account-Manager-1.0.0-alpha.19-mac.dmg',
+        `https://github.com/thenewboston-developers/Account-Manager/releases/download/v1.0.0-alpha.${latestReleaseNumber}/TNB-Account-Manager-1.0.0-alpha.${latestReleaseNumber}-mac.dmg`,
       )}
       {renderOS(
         'Linux',
-        'https://github.com/thenewboston-developers/Account-Manager/releases/download/v1.0.0-alpha.19/TNB-Account-Manager-1.0.0-alpha.19-linux.AppImage',
+        `https://github.com/thenewboston-developers/Account-Manager/releases/download/v1.0.0-alpha.${latestReleaseNumber}/TNB-Account-Manager-1.0.0-alpha.${latestReleaseNumber}-linux.AppImage`,
       )}
     </div>
   );
