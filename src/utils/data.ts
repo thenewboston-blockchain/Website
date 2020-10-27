0b05d8eb4198094f1c03f4d1374bdedac9e663a1
Devan Thakur <devanthakur89@gmail.com>
2020-10-27 18:08:52 +0530

    Team page with mock data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,10 +1,12 @@
 import parse from 'date-fns/parse';
 import {Contributor, RawTask, Task, TaskDict} from 'types/github';
 import {Opening} from 'types/openings';
+import {TeamMember} from 'types/teams';
 
 import contributors from 'data/contributors.json';
 import openings from 'data/openings.json';
 import tasks from 'data/tasks.json';
+import teams from 'data/teams.json';
 
 export const getContributors = (): Contributor[] => {
   return contributors;
@@ -27,4 +29,44 @@ export const getTasks = (): TaskDict => {
   });
 
   return results;
+};
+
+export const getTeamMembers = (): TeamMember[] => {
+  const members: any = {};
+  teams.forEach((team) => {
+    const {title: teamTitle, contributors: teamContributors} = team;
+    teamContributors.forEach((teamMember) => {
+      const {title: userTitle, isLead, payPerDay, createdDate, contributor} = teamMember;
+      const {contributorId, ...otherProps} = contributor;
+      if (!members[contributorId]) {
+        members[contributorId] = {
+          createdDate,
+          isLead,
+          payPerDay,
+          teams: [{isLead, title: teamTitle}],
+          titles: [userTitle],
+          ...otherProps,
+        };
+      } else {
+        const member = members[contributorId];
+        const {teams: addedTeams, titles, isLead: isLeadForAddedTeams} = member;
+        const teamNameExists =
+          addedTeams.findIndex(({title}: {title: string}) => title.toLowerCase() === teamTitle.toLowerCase()) !== -1;
+        const titleExists = titles.findIndex((title: string) => title.toLowerCase() === userTitle.toLowerCase()) !== -1;
+        members[contributorId] = {
+          ...member,
+          isLead: isLeadForAddedTeams || isLead,
+          teams: teamNameExists ? addedTeams : addedTeams.concat([{isLead, title: teamTitle}]),
+          titles: titleExists ? titles : titles.concat([userTitle]),
+        };
+      }
+    });
+  });
+
+  return Object.entries(members)
+    .sort(
+      ([, a]: [string, any], [, b]: [string, any]) =>
+        new Date(b.createdDate).valueOf() - new Date(a.createdDate).valueOf(),
+    )
+    .map(([, member]) => member) as TeamMember[];
 };
