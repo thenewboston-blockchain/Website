5084cb4ed6ab9d40681c518ed21a8f13dc96b494
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-10-27 11:08:53 +0530

    feat: FooterNavList component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,68 @@
 import React, {FC} from 'react';
 import {Link} from 'react-router-dom';
 
-import {SocialMedia} from 'types/social-media';
-
 import Logo from 'assets/svgs/thenewboston-white.svg';
 
-import './Footer.scss';
+import {SocialMedia} from 'types/social-media';
+
+import FooterNavList from './FooterNavList';
 import SocialMediaIcon from './SocialMediaIcon';
+
+import './Footer.scss';
+
+const navLists = [
+  {
+    header: 'Get Started',
+    links: [
+      {
+        title: 'Documentation',
+        url: '/guide/introduction',
+      },
+      {
+        title: 'Tutorials',
+        url: '/tutorials',
+      },
+      {
+        title: 'Tasks',
+        url: '/tasks/All',
+      },
+      {
+        title: 'Download',
+        url: '/download',
+      },
+    ],
+  },
+  {
+    header: 'Community',
+    links: [
+      {
+        title: 'Join the Community!',
+        url: '/community',
+      },
+      {
+        title: 'Openings',
+        url: '/openings',
+      },
+      {
+        title: 'Team',
+        url: '/team',
+      },
+      {
+        title: 'Leaderboard',
+        url: '/leaderboard/All',
+      },
+    ],
+  },
+  {
+    header: 'More',
+    links: [
+      {
+        title: 'FAQ',
+        url: '/',
+      },
+    ],
+  },
+];
 
 const Footer: FC = () => {
   const renderSocialMediaLinks = () =>
@@ -20,16 +76,18 @@ const Footer: FC = () => {
       SocialMedia.twitter,
     ].map((website) => <SocialMediaIcon className="Footer__social-media-link" website={website} />);
 
+  const renderNavLists = () => navLists.map((list) => <FooterNavList header={list.header} links={list.links} />);
+
   return (
-    <div className="Footer">
+    <footer className="Footer">
       <div className="Footer__left">
         <Link to="/">
-          <img className="Footer__logo" src={Logo} alt="thenewboston logo" />
+          <img src={Logo} alt="thenewboston logo" />
         </Link>
         <div className="Footer__social-media-links">{renderSocialMediaLinks()}</div>
       </div>
-      <div className="Footer__right">Nav</div>
-    </div>
+      <div className="Footer__right">{renderNavLists()}</div>
+    </footer>
   );
 };
 
