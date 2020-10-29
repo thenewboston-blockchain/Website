1f3dac220d0813e73167c0a0d2c88a44505d74c6
Justin Kim <justinkim943@gmail.com>
2020-10-28 19:16:36 -0700

    283 - no periods

--- src/containers/Home/HomeHero/index.tsx
+++ src/containers/Home/HomeHero/index.tsx
@@ -15,15 +15,15 @@ const HomeHero: FC = () => {
         <div className="HomeHero__left">
           <div className="HomeHero__left-content-container">
             <h1 className="HomeHero__title">
-              <strong>Build</strong> software.
+              <strong>Build</strong> software
               <br />
               <span className="HomeHero__earn-text">
                 <strong>Earn</strong>
               </span>{' '}
-              digital currency.
+              digital currency
             </h1>
             <h2 className="HomeHero__subtitle">
-              Join our development community and start earning points, our open source digital currency.
+              Join our development community and start earning points, our open source digital currency
             </h2>
             <div className="HomeHero__marketing-buttons">
               <MarketingButtonCircular className="HomeHero__MarketingButton" website={SocialMediaIcon.slack} />
