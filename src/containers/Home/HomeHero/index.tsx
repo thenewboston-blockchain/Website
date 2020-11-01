f231b8d7e2fea573d565c099c31a908cae776b69
Aayushi Maheshwari <aayushi14@users.noreply.github.com>
2020-10-31 23:34:29 -0500

    updated homehero header, subtext, image

--- src/containers/Home/HomeHero/index.tsx
+++ src/containers/Home/HomeHero/index.tsx
@@ -5,6 +5,7 @@ import {SocialMedia} from 'types/social-media';
 
 import DesktopUI from './DesktopUI.png';
 import SendPointsModal from './SendPointsModal.png';
+import WebDesign from './WebDesign.svg';
 
 import './HomeHero.scss';
 
@@ -20,22 +21,17 @@ const HomeHero: FC = () => {
         <div className="HomeHero__left">
           <div className="HomeHero__left-content-container">
             <h1 className="HomeHero__title">
-              <strong>Build</strong> software
-              <br />
-              <span className="HomeHero__earn-text">
-                <strong>Earn</strong>
-              </span>{' '}
-              digital currency
+              <strong>Open-source community for creators</strong>
             </h1>
             <h2 className="HomeHero__subtitle">
-              Join our development community and start earning points, our open source digital currency
+              Learn to code, collaborate on projects, gain experience, build a community, and earn coins by
+              contributing.
             </h2>
             <div className="HomeHero__social-media-links">{renderSocialMediaLinks()}</div>
           </div>
         </div>
         <div className="HomeHero__right">
-          <img alt="desktop" className="HomeHero__desktop-ui" src={DesktopUI} />
-          <img alt="send points modal" className="HomeHero__send-points-modal" src={SendPointsModal} />
+          <img alt="web design" className="HomeHero__web-design" src={WebDesign} />
         </div>
       </div>
     </div>
