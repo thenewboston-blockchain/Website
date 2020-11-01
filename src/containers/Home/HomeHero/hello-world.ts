7e86ebf8403a242619b806e0a237d2325267c148
Justin Kim <justinkim943@gmail.com>
2020-11-01 15:48:11 -0800

    351 - ukraine hello world

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -2,7 +2,6 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   en: 'Hello World',
   ko: '여보세요 세계',
   uk: 'Привіт, світе',
-  ru: 'Здравствуй, мир',
 };
 
 export const defaultHelloWorld = HelloWorld.en;
