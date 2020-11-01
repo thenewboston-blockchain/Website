ec5868db9fee0f287b9c76c5c2207b0a23818c1d
Volodymyr Tartynskyi <vosi@users.noreply.github.com>
2020-11-02 01:29:44 +0200

    Hello world in UK and RU

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -1,6 +1,8 @@
 const HelloWorld: {[isoLangCode: string]: string} = {
   en: 'Hello World',
   ko: '여보세요 세계',
+  uk: 'Привіт, світе',
+  ru: 'Здравствуй, мир',
 };
 
 export const defaultHelloWorld = HelloWorld.en;
