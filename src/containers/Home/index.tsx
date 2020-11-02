b120ba3a4925a74d27ccdcb103057c33aba27a92
Justin Kim <justinkim943@gmail.com>
2020-11-01 17:40:46 -0800

    330 - center layout

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -6,15 +6,13 @@ import HomeHero from './HomeHero';
 import HomeInstantTransactions from './HomeInstantTransactions';
 import HomeSteps from './HomeSteps';
 
-import './Home.scss';
-
 const Home: FC = () => (
-  <div className="Home">
+  <>
     <HomeHero />
     <HomeSteps />
     <HomeInstantTransactions />
     <Footer />
-  </div>
+  </>
 );
 
 export default Home;
