64161619ecf8c2d8cc59d284457b3806fb41606d
Lakshya Thakur <lapstjup@gmail.com>
2020-11-03 22:04:20 +0530

    Revert "Added scroll to top support for Layout"
    This reverts commit 263753479c25ceb467beeede1b4944e78749b8e6.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,7 +3,6 @@ import {useLocation} from 'react-router-dom';
 import clsx from 'clsx';
 
 import {Footer, TopNav} from 'components';
-import {useScrollToTopContainer} from 'hooks';
 import './Layout.scss';
 
 interface ComponentProps {
@@ -12,11 +11,10 @@ interface ComponentProps {
 
 const Layout: FC<ComponentProps> = ({children}) => {
   const {pathname} = useLocation();
-  const layoutDiv = useScrollToTopContainer<HTMLDivElement>([pathname]);
   const isHomepage = pathname === '/';
 
   return (
-    <div className="Layout" ref={layoutDiv}>
+    <div className="Layout">
       <div className="Layout__top-nav-wrapper">
         <TopNav className="Layout__TopNav" />
       </div>
