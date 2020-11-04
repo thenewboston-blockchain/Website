0507d586a9b60cd964bb9a8b403306844421f7fe
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-11-04 17:51:18 +0530

    refactor: redirect /openings to /openings/All

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -54,6 +54,7 @@ const App: FC = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/faq" component={Faq} />
           <Route exact path="/leaderboard/:repository" component={Leaderboard} />
+          <Redirect exact from="/openings" to="/openings/All" />
           <Route exact path="/openings/:category/:openingId?" component={Openings} />
           <Route exact path="/social" component={Social} />
           <Redirect exact from="/tasks" to="/tasks/All" />
