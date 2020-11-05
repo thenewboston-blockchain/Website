41ba0c512fe69da50587a572e5ae51fbd128f299
Velibor <veliborvasiljevic7@gmail.com>
2020-11-05 22:55:33 +0100

    added hello world on serbian

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -12,6 +12,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   uk: 'Привіт, світе',
   ur: 'ہیلو ورلڈ',
   vi: 'Xin chào thế giới',
+  rs: 'Zdravo Svete',
 };
 
 export const defaultHelloWorld = HelloWorld.en;
