210fcc33e42a6980d3120301274025d0c60487c6
Fady Adal <2masadel@gmail.com>
2020-11-05 22:58:45 +0200

    added arabic hero

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -12,6 +12,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   uk: 'Привіт, світе',
   ur: 'ہیلو ورلڈ',
   vi: 'Xin chào thế giới',
+  ar: 'مرحبًا بالعالم'
 };
 
 export const defaultHelloWorld = HelloWorld.en;
