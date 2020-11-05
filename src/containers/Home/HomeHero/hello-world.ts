0dba0286559fb7aa5e46c83d05af036e82d399fd
VJKLFT <vinesh.koelfat@gmail.com>
2020-11-05 22:43:11 +0100

    Update hello-world.ts
    Added the dutch translation for "Hello World" at /src/containers/Home/HomeHero/hello-world.ts for the ISO 639-1 code: nl

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -12,6 +12,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   uk: 'Привіт, світе',
   ur: 'ہیلو ورلڈ',
   vi: 'Xin chào thế giới',
+  nl: 'Hallo Wereld',
 };
 
 export const defaultHelloWorld = HelloWorld.en;
