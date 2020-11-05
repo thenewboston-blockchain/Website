5a9395ac0ce0e77753c18d32ad7a1c56df3c346a
Ramin RX7 <52785580+Ramin-RX7@users.noreply.github.com>
2020-11-06 00:30:30 +0330

    Add Persian (Fa) to hello worlds

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -12,6 +12,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   uk: 'Привіт, світе',
   ur: 'ہیلو ورلڈ',
   vi: 'Xin chào thế giới',
+  fa: 'سلام دنیا'
 };
 
 export const defaultHelloWorld = HelloWorld.en;
