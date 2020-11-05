bd06f45ce15992e3a177f985b59c56e2b12a6e62
ganeshvicky <jackganesh1999@gmail.com>
2020-11-06 02:34:34 +0530

    Update hello-world.ts

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -12,6 +12,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   uk: 'Привіт, світе',
   ur: 'ہیلو ورلڈ',
   vi: 'Xin chào thế giới',
+  ta: 'ஹலோ வேர்ல்ட்',
 };
 
 export const defaultHelloWorld = HelloWorld.en;
