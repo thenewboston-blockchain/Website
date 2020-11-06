56179c494d8bc6d487b67748975ecfbc34a40d9b
Kenan7 <kenankzmv@gmail.com>
2020-11-06 17:16:59 +0400

    remove turkish

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -27,7 +27,6 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   sv: 'Hej Värld',
   ta: 'ஹலோ வேர்ல்ட்',
   te: 'హలో ప్రపంచం',
-  tr: 'Merhaba Dünya',
   uk: 'Привіт світе',
   ur: 'ہیلو ورلڈ',
   vi: 'Xin chào thế giới',
