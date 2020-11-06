9981fadfa0a4b0d1de5c25f408a5e00083196502
Mohammed Moussa <binmosa@gmail.com>
2020-11-06 10:43:58 +0800

    hello world in arabic by BINMOSA

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -12,6 +12,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   uk: 'Привіт, світе',
   ur: 'ہیلو ورلڈ',
   vi: 'Xin chào thế giới',
+  ar: 'مرحبا بالعالم'
 };
 
 export const defaultHelloWorld = HelloWorld.en;
