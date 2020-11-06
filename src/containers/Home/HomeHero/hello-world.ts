67c7e7e02f58f82c6f4a6a2df143444b823d6225
sarveshkolte <49856843+sarveshkolte@users.noreply.github.com>
2020-11-06 10:58:08 +0530

    Add hello world in japanese

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -14,6 +14,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   hi: 'नमस्ते दुनिया',
   id: 'Halo Dunia',
   it: 'Ciao Mondo',
+  ja: 'こんにちは世界',
   ko: '여보세요 세계',
   mk: 'Здраво Свету',
   nl: 'Hallo Wereld',
