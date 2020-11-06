f45a5445edd6cf557466ed502c97a920d5cad945
dwkim922 <dwkim922@gmail.com>
2020-11-06 09:43:02 +0900

    The Korean was wrong, seemed like it was done with a translator. So I changed it to the correct term, since Ive learnt programming in Korean before Im certain that this is right.

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -6,7 +6,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   he: 'שלום עולם',
   hi: 'नमस्ते दुनिया',
   id: 'Halo Dunia',
-  ko: '여보세요 세계',
+  ko: '안녕 세계야',
   mk: 'Здраво Свету',
   te: 'హలో ప్రపంచం',
   uk: 'Привіт, світе',
