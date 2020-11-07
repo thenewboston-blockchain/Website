d6db3b32bb92c0ce7403a1abc0a2a1bdb923a903
piysuh arroa <piyush.arora@cybernetyx.com>
2020-11-07 22:05:21 +0530

    pain-point to pain-points typo fix

--- src/containers/StyleGuide/StyleGuideCss/index.tsx
+++ src/containers/StyleGuide/StyleGuideCss/index.tsx
@@ -258,8 +258,8 @@ const LeftMenu: FC = () => {
       </DocSubSection>
       <DocSubSection id={StyleGuideCssNav.blocksElements} title="Blocks vs. Elements">
         <p>
-          One of the pain-point of <DocInlineCode>BEM</DocInlineCode> is the ambiguity of the distinction between Blocks
-          and Elements. In order to keep it simple, this is the rule we are going to follow:
+          One of the pain-points of <DocInlineCode>BEM</DocInlineCode> is the ambiguity of the distinction between
+          Blocks and Elements. In order to keep it simple, this is the rule we are going to follow:
         </p>
         <DocList variant="ul">
           <li>
