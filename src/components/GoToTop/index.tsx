0531f5db22107772ad61dbe5b00f0a6cc15e59df
manishram <rammanish630@gmail.com>
2020-11-08 03:16:01 +0530

    Changed up arrow to chevron

--- src/components/GoToTop/index.tsx
+++ src/components/GoToTop/index.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import GoToTopIcon from 'mdi-react/ArrowUpIcon';
+import React, {useState} from 'react';
+import GoToTopIcon from 'mdi-react/ChevronUpIcon';
 
 import './GoToTop.scss';
 
-const GoToTopComp = () => {
+const GoToTopButton = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 100) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 100) {
+      setShowScroll(false);
+    }
+  };
+
   const scrollTop = () => {
     window.scrollTo({behavior: 'smooth', top: 0});
   };
+
+  window.addEventListener('scroll', checkScrollTop);
+
   return (
     <div>
       <GoToTopIcon color="#fff" />
-      <GoToTopIcon className="GoToTop__go-to-top" onClick={scrollTop} size={16} />
+      <GoToTopIcon
+        className="GoToTop__go-to-top"
+        onClick={scrollTop}
+        size={16}
+        style={{display: showScroll ? 'flex' : 'none'}}
+      />
       <GoToTopIcon size="1em" />
     </div>
   );
 };
-export default GoToTopComp;
+export default GoToTopButton;
