c68d3d1395eff44166dfb12a1f7587f2f8c4b39f
unknown <nehalchoraria09@gmail.com>
2020-11-07 07:09:09 +0530

    hello world in bengali

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -30,6 +30,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   vi: 'Xin chào thế giới',
   yo: 'Mo ki Ile Aiye',
   zh: '你好 世界',
+  bg : 'ওহে বিশ্ব',
 };
 
 export const defaultHelloWorld = HelloWorld.en;
