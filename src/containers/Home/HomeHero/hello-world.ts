b1a0af6b955c59151c70c5dccb137bb278e6a196
rahulkiller104 <56788144+rahulkiller104@users.noreply.github.com>
2020-11-07 22:55:11 +0530

    updated hello-world.ts
    added "hello world" in Hungarian

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -40,6 +40,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   vi: 'Xin chào thế giới',
   yo: 'Mo ki Ile Aiye',
   zh: '你好 世界',
+  hg:'Helló Világ'
 };
 
 export const defaultHelloWorld = HelloWorld.en;
