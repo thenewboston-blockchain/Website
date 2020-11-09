139290d54726c1d786fbdff76ccc9bd28a70da4d
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-11-09 23:05:34 +0530

    feat: added Albanian language

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -48,6 +48,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   sd: 'هيلو دنيا',
   si: 'හෙලෝ ලෝකය',
   sk: 'Zdravo Svete',
+  sq: 'Përshendetje Botë',
   sr: 'Хало Свет',
   sv: 'Hej Värld',
   sw: 'Habari Dunia',
