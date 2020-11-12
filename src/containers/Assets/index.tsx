20fc36c9861feb16e758733922f4a7729e7ed368
Lakshya Thakur <lapstjup@gmail.com>
2020-11-12 10:24:11 +0530

    More refactoring jsx and css

--- src/containers/Assets/index.tsx
+++ src/containers/Assets/index.tsx
@@ -2,7 +2,7 @@ import React, {FC, memo, ReactNode} from 'react';
 import clsx from 'clsx';
 
 import {Asset} from 'types/assets';
-import {Button, Icon, IconType} from 'components';
+import {A, Icon, IconType} from 'components';
 import {socialMediaUrls} from 'utils/social-media';
 import TnbLogo from 'assets/images/TNB-Logo.png';
 import TnbLogoAndWordmark from 'assets/images/TNB-LogoAndWordmark.png';
@@ -26,15 +26,11 @@ const Assets: FC = () => {
   const renderCard = (asset: Asset): ReactNode => {
     return (
       <div key={asset.subtext} className="Assets__card-wrapper">
-        <div className="Assets__logo-container">
-          <a href={asset.downloadLink} rel="noreferrer" download>
-            <Button className="Assets__downloadButton">
-              <Icon icon={IconType.downloadIcon} size={24} />
-            </Button>
-          </a>
-        </div>
+        <A className="Assets__download-button" href={asset.downloadLink} target="_self">
+          <Icon icon={IconType.downloadIcon} />
+        </A>
         <div className="Assets__card">
-          <img alt={`tnb logo`} className={clsx('Assets__logo', `Assets__logo--tnb`)} src={asset.imageUrl} />
+          <img alt={`tnb logo`} className={clsx('Assets__logo')} src={asset.imageUrl} />
         </div>
         <h2 className="Assets__subtext">{asset.subtext}</h2>
       </div>
@@ -45,7 +41,7 @@ const Assets: FC = () => {
     <div className="Assets">
       <h1 className="Assets__heading">Download thenewboston assets</h1>
       <h2 className="Assets__subtext">All assets of thenewboston at one place for you to download.</h2>
-      <div className="Assets__cards-container">{assets.map((asset: Asset) => renderCard(asset))};</div>
+      <div className="Assets__cards-container">{assets.map((asset: Asset) => renderCard(asset))}</div>
     </div>
   );
 };
