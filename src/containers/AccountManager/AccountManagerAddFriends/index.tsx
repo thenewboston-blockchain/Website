26164791a8345d447ceea8ae62af806d04a53ff4
Hristijan <ilieski.hristijan@gmail.com>
2020-11-13 11:42:56 +0100

    Remove repeating text
    Removet repeating text & added a scentence to explain how to edit / remove a friend.

--- src/containers/AccountManager/AccountManagerAddFriends/index.tsx
+++ src/containers/AccountManager/AccountManagerAddFriends/index.tsx
@@ -14,18 +14,16 @@ const AccountManagerAddFriends: FC = () => {
     <DocContainer className="AccountManagerAddFriends" title="Add Friends">
       <p>
         To add friends, click the plus button to the right of "Friends" on the left side menu. After entering their
-        nickname and account number, click "Add" to add them to your friends list. Once added, you will be taken to
-        their account overview page where you can view their balance, look through their transaction history, and send
-        them coins.
+        nickname and account number, click "Add" to add them to your friends list.
       </p>
 
       <DocImage alt="add friend" bordered maxWidth={1200} src={AddFriend} />
 
       <DocSubSection id={AccountManagerAddFriendsNav.friendOverview} title="Friend Overview">
         <p>
-          After adding a friend, you will be taken to your friend's account overview page. Here you can see their
-          balance, account number, and signing key. But it is your account number that you will share with anyone who
-          wishes to send you coins.
+          After adding a friend, you will be taken to their account overview page where you can view their balance, look
+          through their transaction history, and send them coins. You can also edit or remove friends by using the
+          options menu in the top roght corner.
         </p>
 
         <DocImage alt="friend overview" bordered maxWidth={1200} src={FriendOverview} />
