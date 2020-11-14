bdbb26fb857e363e291793210473f7a437cc1aec
Weav3r <66331153+Weav3r@users.noreply.github.com>
2020-11-14 21:55:25 +0000

    Add Hello World Twi (Akan) (#820)
    Twi (Akan), also known as Akan Kasa, is a dialect of the Akan language spoken in southern and central Ghana by several million people, mainly of the Akan people, the largest of the seventeen major ethnic groups in Ghana (West Africa).
    
    My Account Number:
    c6bfc60493c5bd1ca9289069c098d8ab70ac27d89344de5ede54d25865678090

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -79,6 +79,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   tl: 'Kamusta Mundo',
   tn: 'Dumelang Lefatshe',
   tr: 'Merhaba Dünya',
+  tw: 'Agoo Awiase',
   uk: 'Привіт світе',
   ur: 'ہیلو ورلڈ',
   uz: 'Salom Dunyo',
