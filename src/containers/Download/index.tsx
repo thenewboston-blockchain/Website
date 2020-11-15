b90a311e9b8163c3711f73c60ba1ee0f80694579
Nemanja Paroski <paroskidev@gmail.com>
2020-11-15 14:40:44 +0100

    Removed double focus.

--- src/containers/Download/index.tsx
+++ src/containers/Download/index.tsx
@@ -119,7 +119,7 @@ const Download: FC = () => {
   const renderTabPanel = useCallback(
     (os: Os) => (
       <div className="Download__tab-panel">
-        <a className="Download__download-link" href={getDownloadLink(os)}>
+        <a className="Download__download-link" href={getDownloadLink(os)} tabIndex={-1}>
           <Button className="Download__download-button" disabled={!latestReleaseNumber}>
             Download for {os} <Icon className="Download__download-icon" icon={IconType.arrowCollapseDown} size={18} />
           </Button>
