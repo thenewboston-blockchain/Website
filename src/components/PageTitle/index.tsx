dcbddee2a5697c1bb34f1d450be9102f987e4f90
Lakshya Thakur <lapstjup@gmail.com>
2020-11-16 20:29:45 +0530

    Revert "set defer to false for title change reflection"
    This reverts commit 9a1be18f7b421aec11a8a5c9dddb5818b2f97291.

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -7,7 +7,7 @@ interface ComponentProps {
 
 const PageTitle: FC<ComponentProps> = ({title}) => {
   return (
-    <Helmet defer={false}>
+    <Helmet>
       <title>{title} | thenewboston</title>
     </Helmet>
   );
