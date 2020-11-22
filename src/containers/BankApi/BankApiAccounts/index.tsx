a8f719259382e31a836a41b9c9c5be256f17d2c6
Velibor <veliborvasiljevic7@gmail.com>
2020-11-22 12:08:28 +0100

    accounts get

--- src/containers/BankApi/BankApiAccounts/index.tsx
+++ src/containers/BankApi/BankApiAccounts/index.tsx
@@ -18,25 +18,31 @@ const BankApiAccounts: FC = () => {
 
       <DocEndpoint endpoint="/accounts" method="GET" />
       <RequestResponseSnippet
-        code={`[
-  {
-    "id": "9eca00a5-d925-454c-a8d6-ecbb26ec2f76",
-    "created_date": "2020-07-08T02:14:59.307535Z",
-    "modified_date": "2020-07-08T02:14:59.307553Z",
-    "account_number": "4d2ec91f37bc553bc538e91195669b666e26b2ea3e4e31507e38102a758d4f86",
-    "trust": "75.21"
-  },
-  {
-    "id": "ae4d43b0-5c34-4e56-8266-0e3531268815",
-    "created_date": "2020-07-08T02:15:12.271834Z",
-    "modified_date": "2020-07-08T02:15:12.271852Z",
-    "account_number": "a29baa6ba36f6db707f8f8dacfa82d5e8a28fa616e8cc96cf6d7790f551d79f2",
-    "trust": "94.63"
-  }
-]`}
+        code={`{
+  "count": 2,
+  "next": "http://143.110.137.54/accounts?limit=50&offset=50",
+  "previous": null,
+  "results": [
+    {
+      "id": "5a8c7990-393a-4299-ae92-2f096a2c7f43",
+      "created_date": "2020-10-08T02:18:07.346849Z",
+      "modified_date": "2020-10-08T02:18:07.346914Z",
+      "account_number": "a37e2836805975f334108b55523634c995bd2a4db610062f404510617e83126f",
+      "trust": "0.00"
+    },
+    {
+      "id": "2682963f-06b1-47d7-a2e1-1f8ec6ae98dc",
+      "created_date": "2020-10-08T02:39:44.071810Z",
+      "modified_date": "2020-10-08T02:39:44.071853Z",
+      "account_number": "cc8fb4ebbd2b9a98a767e801ac2b0d296ced88b5d3b7d6d6e12e1d2d7635d724",
+      "trust": "0.00"
+    }
+}`}
         heading="Response"
       />
 
+      {/** 
+       
       <DocEndpoint endpoint="/accounts/<account_number>" method="PATCH" />
       <TableParams
         items={[
@@ -49,8 +55,8 @@ const BankApiAccounts: FC = () => {
       />
       <RequestResponseSnippet
         code={`{
-  "message": {
-    "trust": 99.98
+          "message": {
+            "trust": 99.98
   },
   "node_identifier": "d5356888dc9303e44ce52b1e06c3165a7759b9df1e6a6dfbd33ee1c3df1ab4d1",
   "signature": "f41788fe19690a67abe3336d4ca84565c090691efae0e5cdd8bf02e126842215080405013b8461f734d091e673e9edefca53a51773fda59bbebcef77ab8e2901"
@@ -59,7 +65,7 @@ const BankApiAccounts: FC = () => {
       />
       <RequestResponseSnippet
         code={`{
-  "id": "64426fc5-b3ac-42fb-b75b-d5ccfcdc6872",
+          "id": "64426fc5-b3ac-42fb-b75b-d5ccfcdc6872",
   "created_date": "2020-07-14T02:59:22.204580Z",
   "modified_date": "2020-07-21T00:58:01.013685Z",
   "account_number": "0cdd4ba04456ca169baca3d66eace869520c62fe84421329086e03d91a68acdb",
@@ -67,6 +73,7 @@ const BankApiAccounts: FC = () => {
 }`}
         heading="Response"
       />
+      */}
     </DocContainer>
   );
 };
