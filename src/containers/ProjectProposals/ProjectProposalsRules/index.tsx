3f90e0b71b3cf71251e3905010114990f7e3dc1c
manishram <rammanish630@gmail.com>
2020-11-23 10:43:20 +0530

    added point number 6

--- src/containers/ProjectProposals/ProjectProposalsRules/index.tsx
+++ src/containers/ProjectProposals/ProjectProposalsRules/index.tsx
@@ -35,6 +35,11 @@ const ProjectProposalsRules: FC = () => {
       />
       <StepIndicator
         className="ProjectProposalsRules__StepIndicator"
+        number={6}
+        text="Projects promoting pedophilia or inappropriate interaction targeted at a minor are not allowed."
+      />
+      <StepIndicator
+        className="ProjectProposalsRules__StepIndicator"
         number={7}
         text="Projects which use the user's personal information for any purpose must take prior permission from the user or inform them clearly."
       />
