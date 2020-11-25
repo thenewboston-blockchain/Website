b9c994b985090b78411d8ec3f0408fb9503c4a2c
Duc Pham <phamcongduc1994@gmail.com>
2020-11-25 21:12:45 +0700

    fixup! Update document for Bank API /Banks

--- src/containers/BankApi/BankApiBanks/index.tsx
+++ src/containers/BankApi/BankApiBanks/index.tsx
@@ -16,32 +16,46 @@ const BankApiBanks: FC = () => {
         guide.
       </p>
 
-      <DocEndpoint endpoint="/banks" method="GET" />
+      <DocEndpoint endpoint="/banks?limit=2&offset=4" method="GET" />
+      <TableParams
+        items={[
+          {
+            dataType: 'URL parameter',
+            description: 'specific point in banks list will be returned.',
+            param: 'offset',
+          },
+          {
+            dataType: 'URL parameter',
+            description: 'Maximum number of banks returned by the node.',
+            param: 'limit',
+          },
+        ]}
+      />
       <RequestResponseSnippet
         code={`{
-  "count": 2,
-  "next": null,
-  "previous": null,
+  "count": 17,
+  "next": "http://143.110.137.54/banks?limit=2&offset=6",
+  "previous": "http://143.110.137.54/banks?limit=2&offset=2",
   "results": [
     {
-      "account_number": "336dfdf1eacb968511da967b2289e39361a33b823ef2cfabb9cd818b3110e2ee",
-      "ip_address": "3.35.3.48",
-      "node_identifier": "35f4c988f425809ca7f5d0b319cdf8f7d7aba1b064fd0efc85d61fa0f4d05145",
+      "account_number": "da8500cb8e2ffd728f919cfae82b1c4e97ca2558f2545ab1b020a4172642dce3",
+      "ip_address": "34.202.233.224",
+      "node_identifier": "3d6de056dc9ecbca2b4c832017dcb5dbdc2c95dd3175244acf7dfbc21add76de",
       "port": 80,
       "protocol": "http",
       "version": "v1.0",
       "default_transaction_fee": 1,
-      "trust": "100.00"
+      "trust": "0.00"
     },
     {
-      "account_number": "dfddf07ec15cbf363ecb52eedd7133b70b3ec896b488460bcecaba63e8e36be5",
-      "ip_address": "143.110.137.54",
-      "node_identifier": "6dbaff44058e630cb375955c82b0d3bd7bc7e20cad93e74909a8951f747fb8a4",
-      "port": null,
+      "account_number": "c4caa42b2a01b31ee187468ac63bd64745f67ec3b20191a54eb55ba20d5adbb0",
+      "ip_address": "18.191.29.186",
+      "node_identifier": "8990b681d8d14b3bf2cd38782c6053bb365cc54616f06ec8d88d9dadb8aa0780",
+      "port": 80,
       "protocol": "http",
       "version": "v1.0",
       "default_transaction_fee": 1,
-      "trust": "12.34"
+      "trust": "0.00"
     }
   ]
 }`}
@@ -61,7 +75,7 @@ const BankApiBanks: FC = () => {
       <RequestResponseSnippet
         code={`{
   "message": {
-    "trust": "76.43"
+    "trust": 76.43
   },
   "node_identifier": "35f4c988f425809ca7f5d0b319cdf8f7d7aba1b064fd0efc85d61fa0f4d05145",
   "signature": "93952df29ae3885fd9c9f88721314236bdb53ca5632b2959dcf5cf3c38cb8b96ca57ff84c5337eb164f803237f901abcb0c41a9f71e14aa2fb3159c7ad7a7509"
