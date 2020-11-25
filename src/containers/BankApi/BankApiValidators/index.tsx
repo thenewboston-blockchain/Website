a3f044cacf9cbabfc2aab333518ee8321a72e885
Aayushi Maheshwari <aayushi14@users.noreply.github.com>
2020-11-24 18:28:37 -0600

    added query params table

--- src/containers/BankApi/BankApiValidators/index.tsx
+++ src/containers/BankApi/BankApiValidators/index.tsx
@@ -13,10 +13,24 @@ const BankApiValidators: FC = () => {
       </p>
 
       <DocEndpoint endpoint="/validators" method="GET" />
+      <TableParams
+        items={[
+          {
+            dataType: 'URL parameter',
+            description: 'If you want to start at a specific point, an offset can be specified.',
+            param: 'offset',
+          },
+          {
+            dataType: 'URL parameter',
+            description: 'Maximum number of block returned by the node. (max: 100)',
+            param: 'limit',
+          },
+        ]}
+      />
       <RequestResponseSnippet
         code={`{
-    "count": 16,
-    "next": null,
+    "count": 10,
+    "next": "http://54.219.178.46/validators?limit=2&offset=2",
     "previous": null,
     "results": [
         {
@@ -27,25 +41,25 @@ const BankApiValidators: FC = () => {
             "protocol": "http",
             "version": "v1.0",
             "default_transaction_fee": 1,
-            "root_account_file": "https://gist.githubusercontent.com/buckyroberts/0688f136b6c1332be472a8baf10f78c5/raw/323fcd29672e392be2b934b82ab9eac8d15e840f/alpha-00.json",
-            "root_account_file_hash": "0f775023bee79884fbd9a90a76c5eacfee38a8ca52735f7ab59dab63a75cbee1",
+            "root_account_file": "http://54.219.178.46/media/root_account_file.json",
+            "root_account_file_hash": "cc9390cc579dc8a99a1f34c1bea5d54a0f45b27ecee7e38662f0cd853f76744d",
             "seed_block_identifier": "",
-            "daily_confirmation_rate": null,
-            "trust": "0.00"
+            "daily_confirmation_rate": 1,
+            "trust": "100.00"
         },
         {
-            "account_number": "d5c4db217c032ef21df84be4201766b73e623940ce6d95aedf153da2f8c38626",
-            "ip_address": "54.67.72.197",
-            "node_identifier": "61dbf00c2dd7886f01fda60aca6fffd9799f4612110fe804220570add6b28923",
+            "account_number": "2e86f48216567302527b69eae6c6a188097ed3a9741f43cc3723e570cf47644c",
+            "ip_address": "54.183.17.224",
+            "node_identifier": "2262026a562b0274163158e92e8fbc4d28e519bc5ba8c1cf403703292be84a51",
             "port": null,
             "protocol": "http",
             "version": "v1.0",
             "default_transaction_fee": 1,
-            "root_account_file": "https://gist.githubusercontent.com/buckyroberts/0688f136b6c1332be472a8baf10f78c5/raw/323fcd29672e392be2b934b82ab9eac8d15e840f/alpha-00.json",
-            "root_account_file_hash": "0f775023bee79884fbd9a90a76c5eacfee38a8ca52735f7ab59dab63a75cbee1",
+            "root_account_file": "http://54.183.17.224/media/root_account_file.json",
+            "root_account_file_hash": "cc9390cc579dc8a99a1f34c1bea5d54a0f45b27ecee7e38662f0cd853f76744d",
             "seed_block_identifier": "",
             "daily_confirmation_rate": null,
-            "trust": "0.00"
+            "trust": "100.00"
         }
     ]
 }`}
