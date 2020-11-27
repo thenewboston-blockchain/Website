aa67c9c7d6fddba72cfe8d12e4174327044d6b91
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-11-28 00:05:07 +0530

    chore: change route order

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -60,9 +60,9 @@ const App: FC = () => {
           <Route exact path="/donate" component={Donate} />
           <Route exact path="/faq" component={Faq} />
           <Route exact path="/assets" component={Assets} />
+          <Redirect exact from="/leaderboard" to="/leaderboard/All" />
           <Route exact path="/leaderboard/:repository" component={Leaderboard} />
           <Redirect exact from="/openings" to="/openings/All" />
-          <Redirect exact from="/leaderboard" to="/leaderboard/All" />
           <Route exact path="/openings/:category/:openingId?" render={() => <Openings openingsFrozen={false} />} />
           <Route exact path="/social" component={Social} />
           <Redirect exact from="/tasks" to="/tasks/All" />
