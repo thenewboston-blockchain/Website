ab3f4d1c4ee203f1251a1da8529bdb736d249e02
bucky <buckyroberts@gmail.com>
2020-12-13 19:20:40 -0800

    Merge branch 'development' into 1084-add-job-description
    # Conflicts:
    #	src/data/openings.json

--- src/containers/Download/index.tsx
+++ src/containers/Download/index.tsx
@@ -142,7 +142,7 @@ const Download: FC = () => {
       <div className="Download__tab-panel">
         <a className="Download__download-link" href={getDownloadLink(os)} tabIndex={-1}>
           <Button className="Download__download-button" disabled={!latestReleaseNumber}>
-            {`Download  for  ${os}`}
+            <span>Download for {os}</span>
             <Icon className="Download__download-icon" icon={IconType.arrowCollapseDown} size={18} />
           </Button>
         </a>
