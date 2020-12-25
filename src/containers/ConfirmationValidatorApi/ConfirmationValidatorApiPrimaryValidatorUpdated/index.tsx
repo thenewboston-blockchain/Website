0ce7ea3738ff9016a9603fed43a2b7f614df1c56
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2020-12-25 20:05:46 +0500

    Updated NID and signature

--- src/containers/ConfirmationValidatorApi/ConfirmationValidatorApiPrimaryValidatorUpdated/index.tsx
+++ src/containers/ConfirmationValidatorApi/ConfirmationValidatorApiPrimaryValidatorUpdated/index.tsx
@@ -46,8 +46,8 @@ const ConfirmationValidatorApiPrimaryValidatorUpdated: FC = () => {
     "port": "8000",
     "protocol": "http"
   },
-  "node_identifier": "d5356888dc9303e44ce52b1e06c3165a7759b9df1e6a6dfbd33ee1c3df1ab4d1",
-  "signature": "ccb4c9f0669abfcd1cea330fa4e45d87689fb08d2fd26b4cf05c87b2fdb1dae543024888243a84d02fd9bb8320c41f4de45d6212747e49a02a365fd42883fd01"
+  "node_identifier": "ce236a006a0aec488ad8e5161dd018b2ce84d9e4d9adc897eca7fb112864198f",
+  "signature": "0964ba25c6b1433ab921e357c67bfc07fa9b931897e0ecd719e9acccf2f60a5e60501c1e134a1ea1ac4724a42f2d60d441a32a6f42a0bccbef1dc2c5a4c0a200"
 }`}
         heading="Request"
       />
