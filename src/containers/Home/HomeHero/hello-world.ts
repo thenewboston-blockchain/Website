489f2ea6c4a04b2e833d7169808f65cd61277714
Ishmael28 <61079938+Ishmael28@users.noreply.github.com>
2021-02-02 16:25:55 +0200

    Tshivenda
    1d5f3480db8f18df07df190f45ed9a91a393189cd7898fca00a5a179235648b6

--- src/containers/Home/HomeHero/hello-world.ts
+++ src/containers/Home/HomeHero/hello-world.ts
@@ -100,6 +100,7 @@ const HelloWorld: {[isoLangCode: string]: string} = {
   ur: 'ہیلو ورلڈ',
   uz: 'Salom Dunyo',
   vi: 'Xin chào thế giới',
+  ve: 'Ria Vusa Lifhasi',
   wo: 'Salaam aleekum Àdduna',
   xh: 'Molo Lizwe',
   yi: 'העלא וועלט',
