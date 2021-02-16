c6456576ac0479f8fe3137cdf81b02b18618bdd1
led76marx <vrepantis@yahoo.gr>
2021-02-15 23:08:48 -0800

    Editorial pass and callout box on Accounts doc Issue #1298

--- src/containers/Guide/GuideAccounts/index.tsx
+++ src/containers/Guide/GuideAccounts/index.tsx
@@ -1,13 +1,13 @@
 import React, {FC} from 'react';
-
+import {NavLink} from 'react-router-dom';
 import KeyPair from 'assets/images/KeyPair.png';
 import {DocContainer, DocImage, DocList} from 'components';
-
+import './GuideAccounts.scss';
 import AccountsSimple from './AccountsSimple.png';
 
 const GuideAccounts: FC = () => {
   return (
-    <DocContainer className="GuideAccounts" title="Accounts" lastUpdated="07 Dec 2020">
+    <DocContainer className="GuideAccounts" title="Accounts" lastUpdated="16 Feb 2021">
       <p>
         <strong>Accounts</strong> are anonymous digital identities on the network where coins may be sent to and from.{' '}
         <strong>Account managers</strong> are software applications (such as a mobile application or desktop software)
@@ -20,22 +20,21 @@ const GuideAccounts: FC = () => {
       <DocImage alt="accounts simple" maxWidth={600} src={AccountsSimple} />
 
       <p>
-        When users first download an account manager, it will take the user through the process of creating a{' '}
-        <strong>key pair</strong>. A key pair consists of two keys, a <strong>signing key</strong> an d an{' '}
-        <strong>account number</strong>.
+        When users first download an account manager, it guides them through creating a <strong>key pair</strong>. A key
+        pair comprises two keys, a <strong>signing key</strong> and an <strong>account number</strong>.
       </p>
 
       <DocImage alt="key pair" maxWidth={380} src={KeyPair} />
 
       <p>
         The account number of an account is like a username, but is unique across the entire network (rather than a
-        single website). The use of signing keys secures all accounts. Signing keys can prove ownership of coins. They
-        are also used in the process of "signing" a block. This allows for the ability of users to create secure
-        transactions that the network can further validate without revealing the signing key itself. The signing key
-        should always remain private to the account owner.
+        single website). Using signing keys secures all accounts and prove ownership of coins. Signing keys are also
+        used in the process of "signing" a block. This allows users to create secure transactions that the network can
+        further validate without revealing the signing key itself. The signing key must always remain private to the
+        account owner.
       </p>
 
-      <p>A simplified overview of each key is as follows:</p>
+      <p>A simplified overview of each key is the following:</p>
 
       <p>
         <strong>Signing key</strong>
@@ -54,25 +53,28 @@ const GuideAccounts: FC = () => {
         <li>Made public to the network</li>
         <li>Acts as a receiving address</li>
         <ul>
-          <li>Other users can send coins to you using your account number</li>
+          <li>Any user can send coins to the owner of the account using this account number</li>
         </ul>
-        <li>Used in verifying the sender when validating transactions</li>
+        <li>Verifies the sender when validating transactions</li>
         <ul>
           <li>
-            The account number can prove ownership of the signing key, and therefore the account owner, without
+            The account number can prove ownership of the signing key, and consequently the account owner, without
             revealing the signing key itself
           </li>
           <li>
-            The use of asymmetric cryptography (specifically digital signatures) achieves this, which we will cover in a
-            later section
+            Use of asymmetric cryptography (specifically digital signatures) achieves this (see
+            <NavLink to="/guide/blocks"> Blocks</NavLink> for details)
           </li>
         </ul>
       </DocList>
-
       <p>
-        <strong>Important Note:</strong> Users can have more than one account. Users can create and manage several
-        accounts (key pairs) through the same account manager application. This can be thought of as an individual
-        having multiple email addresses, but managing all of their email accounts from a single app.
+        <div className="callout">
+          <p>
+            <strong>Note:</strong> Users can have more than one account, and they can create and manage several accounts
+            (key pairs) through the same account manager application. This can be thought of as an individual having
+            multiple email addresses, but managing all of their email accounts from a single app.
+          </p>
+        </div>
       </p>
       <p>The terms "account" and "user account" are used interchangeably throughout this documentation.</p>
     </DocContainer>
