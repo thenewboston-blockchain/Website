b1405283ea80df78511118f9888f8fb752c6e735
tomijaga <50385769+tomijaga@users.noreply.github.com>
2021-02-21 13:23:58 -0500

    Removed unused Import

--- src/components/RequiredAsterisk/RequiredAsterisk.test.tsx
+++ src/components/RequiredAsterisk/RequiredAsterisk.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
 import RequiredAsterisk from './index';
