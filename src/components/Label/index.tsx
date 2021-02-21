ae93e9bc9fff93752c6609bc410f9a3ed074d986
jamessspanggg <jamespangmunwai@gmail.com>
2021-02-21 18:03:23 +0800

    ! Add tests for Label component

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -4,18 +4,19 @@ import clsx from 'clsx';
 import {isLight} from 'utils/colors';
 import './Label.scss';
 
-interface ComponentProps {
+export interface LabelProps {
   className?: string;
   color: string;
   name: string;
 }
 
-const Label: FC<ComponentProps> = ({className, color, name}) => {
+const Label: FC<LabelProps> = ({className, color, name}) => {
   const hexColor = `#${color}`;
 
   return (
     <span
       className={clsx('Label', className)}
+      data-testid="Label"
       style={{
         backgroundColor: hexColor,
         color: isLight(hexColor) ? '#000' : '#fff',
