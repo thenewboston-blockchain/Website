57b0f648d1559d87dc178347da4ce93a19b2df3b
Justin Kim <justinkim943@gmail.com>
2021-02-21 18:58:34 -0800

    minor clean up

--- src/components/Shadow/Shadow.test.tsx
+++ src/components/Shadow/Shadow.test.tsx
@@ -5,7 +5,6 @@ import '@testing-library/jest-dom/extend-expect';
 import Shadow from './index';
 
 describe('Shadow component', () => {
-  // eslint-disable-next-line jest/expect-expect
   it('renders without crashing', () => {
     render(<Shadow />);
   });
