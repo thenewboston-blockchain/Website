01939720b4dba86e140269b89c14df39ccbd217d
Justin Kim <justinkim943@gmail.com>
2021-02-21 20:22:37 -0800

    remove eslint-disable

--- src/components/FlatNavLinks/FlatNavLinks.test.tsx
+++ src/components/FlatNavLinks/FlatNavLinks.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
