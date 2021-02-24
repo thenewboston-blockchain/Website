4bba6e4fdd17e942c542d0bd7db393a338e9e6f2
James Pang <jamespangmunwai@gmail.com>
2021-02-24 08:26:32 +0800

    Add AuthContainer tests (#1406)

---
+++ src/components/AuthContainer/AuthContainer.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import AuthContainer, {AuthContainerProps} from '.';
+
+describe('AuthContainer', () => {
+  const baseProps: AuthContainerProps = {
+    children: <div>Children</div>,
+    heading: 'Auth Container Heading',
+  };
+
+  it('renders proper default className', () => {
+    render(<AuthContainer {...baseProps} />);
+    const el = screen.getByTestId('AuthContainer');
+
+    expect(el.className).toBe('AuthContainer');
+  });
+
+  it('renders without error passed in', () => {
+    render(<AuthContainer {...baseProps} />);
+
+    expect(screen.getByTestId('AuthContainer')).toBeTruthy();
+    expect(screen.getAllByText('Auth Container Heading')).toBeTruthy();
+    expect(screen.getAllByText('Children')).toBeTruthy();
+    expect(screen.queryByTestId('AuthContainer__error-message')).toBeNull();
+  });
+
+  it('renders with error passed in', () => {
+    render(<AuthContainer {...baseProps} errorMessage="Error" />);
+
+    expect(screen.getByTestId('AuthContainer')).toBeTruthy();
+    expect(screen.getAllByText('Auth Container Heading')).toBeTruthy();
+    expect(screen.getAllByText('Children')).toBeTruthy();
+    expect(screen.getAllByText('Error')).toBeTruthy();
+  });
+});
