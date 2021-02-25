2c36c822d7caf7458267a4482f69e5f45c08a200
Justin Kim <justinkim943@gmail.com>
2021-02-24 19:15:25 -0800

    separate the tests and add unit test

--- src/components/SocialMediaIcon/SocialMediaIcon.test.tsx
+++ src/components/SocialMediaIcon/SocialMediaIcon.test.tsx
@@ -10,25 +10,34 @@ import SocialMediaIcon from './index';
 const SIZE = 16;
 
 describe('SocialMediaIcon component', () => {
-  test('renders with expected props of iconSize 16 and discord as website', () => {
+  test('renders with expected link', () => {
     render(<SocialMediaIcon iconSize={SIZE} website={SocialMedia.discord} />);
-
     const el = screen.getByTestId('A');
-    expect(el).toBeTruthy();
-    expect(el.hasAttribute('href')).toBeTruthy();
-    expect(el.getAttribute('href')).toEqual(socialMediaUrls[SocialMedia.discord]);
 
+    expect(el).toBeTruthy();
+    expect(el.getAttribute('href')).toEqual(socialMediaUrls[SocialMedia.discord]);
+  });
+
+  test('renders with correct iconSize', () => {
+    render(<SocialMediaIcon iconSize={SIZE} website={SocialMedia.facebook} />);
+    const el = screen.getByTestId('A');
     const svgIcon = el.querySelector('.mdi-icon');
+
     expect(svgIcon?.getAttribute('width')).toEqual(SIZE.toString());
     expect(svgIcon?.getAttribute('height')).toEqual(SIZE.toString());
   });
 
-  test('renders socialMediaIcon and get by className', () => {
+  test('renders with default className', () => {
     render(<SocialMediaIcon iconSize={SIZE} website={SocialMedia.facebook} />);
-
     const el = screen.getByTestId('A');
+
     expect(el.className).toContain('SocialMediaIcon');
-    expect(el.hasAttribute('href')).toBeTruthy();
-    expect(el.getAttribute('href')).toEqual(socialMediaUrls[SocialMedia.facebook]);
+  });
+
+  test('renders with className passed in', () => {
+    render(<SocialMediaIcon className="test-class" iconSize={SIZE} website={SocialMedia.facebook} />);
+    const el = screen.getByTestId('A');
+
+    expect(el.className).toContain('test-class');
   });
 });
