0fc47a3af6b5d65941b2dda0b290f2d8fa63e68c
denistsoi <denistsoi@gmail.com>
2021-02-25 12:09:22 +0800

    [#1362] - Marketing button Test (#1405)
    * Test for #1362: Marketing Button;
    
    * ADD: check for className;
    
    * ADD: update test;
    
    * add test for child components
    
    Co-authored-by: Justin Kim <justinkim943@gmail.com>

--- src/components/A/index.tsx
+++ src/components/A/index.tsx
@@ -8,22 +8,23 @@ import './A.scss';
 interface ComponentProps {
   children?: ReactNode;
   className?: string;
+  dataTestId?: string;
   href: string;
   newWindow?: boolean;
 }
 
-const A: FC<ComponentProps> = ({children, className, href, newWindow = true}) => {
+const A: FC<ComponentProps> = ({children, className, dataTestId = 'A', href, newWindow = true}) => {
   const renderIcon = newWindow && typeof children === 'string';
   const rel = newWindow ? 'noreferrer' : undefined;
   const target = newWindow ? '_blank' : '_self';
 
   return (
-    <a className={clsx('A', className)} data-testid="A" href={href} rel={rel} target={target}>
+    <a className={clsx('A', className)} data-testid={dataTestId} href={href} rel={rel} target={target}>
       {children}
       {renderIcon && (
         <Icon
           className={clsx('A__Icon--new-window', {...bemify(className, '__Icon--new-window')})}
-          dataTestId="A__Icon--new-window"
+          dataTestId={`${dataTestId}__Icon--new-window`}
           icon={IconType.openInNew}
           size={16}
           totalSize={20}
