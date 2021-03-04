5d56b9aa89abf1a9b3b02ed7f351b6c5cc4668b7
jamessspanggg <jamespangmunwai@gmail.com>
2021-03-04 21:31:46 +0800

    Add tests for default og meta tags

--- src/components/PageTitle/PageTitle.test.tsx
+++ src/components/PageTitle/PageTitle.test.tsx
@@ -3,7 +3,7 @@ import {HelmetProvider} from 'react-helmet-async';
 import {render, waitFor} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import PageTitle from './index';
+import PageTitle, {defaultOgDescription, defaultOgImageUrl, defaultOgType, defaultOgUrl} from './index';
 
 describe('Page Title component', () => {
   test('renders with text passed in', async () => {
@@ -14,5 +14,24 @@ describe('Page Title component', () => {
     );
 
     await waitFor(() => expect(document.title).toContain('Test | thenewboston'));
+  });
+
+  test('renders with default og meta tags', async () => {
+    render(
+      <HelmetProvider>
+        <PageTitle title="Home" />
+      </HelmetProvider>,
+    );
+
+    await waitFor(() => {
+      expect(document.title).toContain('Home | thenewboston');
+      expect((document.querySelector('[property="og:type"]') as HTMLMetaElement).content).toBe(defaultOgType);
+      expect((document.querySelector('[property="og:title"]') as HTMLMetaElement).content).toBe('Home | thenewboston');
+      expect((document.querySelector('[property="og:description"]') as HTMLMetaElement).content).toBe(
+        defaultOgDescription,
+      );
+      expect((document.querySelector('[property="og:url"]') as HTMLMetaElement).content).toBe(defaultOgUrl);
+      expect((document.querySelector('[property="og:image"]') as HTMLMetaElement).content).toBe(defaultOgImageUrl);
+    });
   });
 });
