d594ca55a71b144260e17ac95e5c3e7b34eea04e
Denis Tsoi <denistsoi@gmail.com>
2021-03-05 22:34:08 +0800

    ADD  select logic;

---
+++ src/components/DropdownInput/DropdownInput.test.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {render, screen, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import DropdownInput from '.';
+
+const BaseProps = {
+  callbackOnChange: () => {},
+  defaultOption: 'second',
+  options: ['first', 'second'],
+};
+
+describe('DropdownInput component', () => {
+  test('renders without crashing', () => {
+    render(<DropdownInput {...BaseProps} />);
+    const el = screen.getByTestId('DropdownInput');
+    expect(el.className).toContain('DropdownInput');
+  });
+
+  test('renders options', () => {
+    render(<DropdownInput {...BaseProps} />);
+    const options = screen.getAllByTestId('DropdownInput__option');
+    expect(options.length).toEqual(BaseProps.options.length);
+  });
+  test('renders options with BaseProps.options value', () => {
+    render(<DropdownInput {...BaseProps} />);
+    const options = screen.getAllByTestId('DropdownInput__option');
+    expect(options.length).toEqual(BaseProps.options.length);
+    options.forEach((option, index) => {
+      expect(option).toHaveValue(BaseProps.options[index]);
+    });
+  });
+
+  test('renders options with classname', () => {
+    render(<DropdownInput {...BaseProps} />);
+    const options = screen.getAllByTestId('DropdownInput__option');
+    expect(options.length).toEqual(BaseProps.options.length);
+
+    options.forEach((option) => {
+      expect(option).toHaveClass('DropdownInput__option');
+    });
+  });
+
+  test('renders Icon with DropdownInput__chevron-down', () => {
+    render(<DropdownInput {...BaseProps} />);
+    const component = screen.getByTestId('DropdownInput');
+    expect(component.querySelector('.DropdownInput__chevron-down')).toBeTruthy();
+  });
+
+  test('renders select box with className DropdownInput__select-box', () => {
+    render(<DropdownInput {...BaseProps} />);
+    expect(screen.getByTestId('DropdownInput__select-box')).toHaveClass('DropdownInput__select-box');
+  });
+
+  // test('change select value that is different to defaultOption', () => {
+  //   render(<DropdownInput {...BaseProps} />);
+  //   fireEvent.click(screen.getByTestId('DropdownInput__select-box'), {
+  //     target: {
+  //       value: 'second',
+  //     },
+  //   });
+
+  //   const options = screen.getAllByTestId('DropdownInput__option');
+  //   expect(options).toHaveFormValues({ food: '' })
+  //   const firstOption = options[0] as HTMLInputElement;
+  //   const secondOption = options[1] as HTMLInputElement;
+  //   expect(firstOption.value).toEqual('first');
+  //   expect(secondOption.value).toEqual('second');
+  //   screen.debug();
+  // });
+});
