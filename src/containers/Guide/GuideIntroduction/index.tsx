6280158d6a6d5843f0b14c6e5b19b899bb7e6b31
led76marx <vrepantis@yahoo.gr>
2021-03-07 14:41:49 -0800

    Stylistic updates - Introduction Guide (#1486)

--- src/containers/Guide/GuideIntroduction/index.tsx
+++ src/containers/Guide/GuideIntroduction/index.tsx
@@ -1,7 +1,7 @@
 import React, {FC, ReactNode} from 'react';
 import {NavLink} from 'react-router-dom';
 
-import {DocContainer, DocImage, TableVertical} from 'components';
+import {DocContainer, DocImage, DocList, DocSubHeader, TableVertical} from 'components';
 
 import OverviewDiagram from './Overview.png';
 
@@ -10,27 +10,24 @@ const GuideIntroduction: FC = () => {
     <TableVertical
       altColors
       rows={[
-        ['Account', 'An anonymous digital identity on the network where coins may be sent to and from'],
-        ['Transaction', 'The transfer of coins from one account to another'],
-        ['Block', "A group of one or more transactions along with the sender's account number and a signature"],
+        ['Account', 'Anonymous digital identity on the network through which a user can send and receive coins'],
+        ['Transaction', 'Transfer of coins from one account to another'],
+        ['Block', "Group of one or more transactions along with the sender's account number and a signature"],
         [
           'Signature',
-          'Unique value used to verify the authenticity of a digital document (included with every block to prove that the account owner has authorized the related transactions)',
+          'Unique value that verifies authenticity of a digital document (included with every block to prove that the account owner has authorized all the related transactions)',
         ],
-        [
-          'Node',
-          'A network server that has several responsibilities such as routing blocks and transaction validation',
-        ],
-        ['Node Block', 'A block that a node has signed to indicate that it conforms to network protocol'],
+        ['Node', 'Network server that has several responsibilities, such as routing blocks and transaction validation'],
+        ['Node Block', 'A block signed by a node indicating that it conforms to network protocol'],
         [
           'Primary Validator (PV)',
-          'A designated node that accepts blocks from other nodes and after successful validation/confirmation of each block, creates a new confirmation block which is added to the blockchain',
+          'Designated node that accepts blocks from other nodes; after successful validation/confirmation of each block, it creates a new confirmation block which is added to the blockchain',
         ],
         [
           'Confirmation',
-          'A block that a node signs as confirmation that it has been added to their blockchain; indicates that the transactions have been validated and that the coins have been successfully transferred',
+          'A block signed by a node to establish that transactions have been added to the blockchain; confirms validation of transactions and successful transfer of coins',
         ],
-        ['Blockchain', 'An ordered list of confirmations'],
+        ['Blockchain', 'Ordered list of confirmations'],
       ]}
     />
   );
@@ -39,8 +36,8 @@ const GuideIntroduction: FC = () => {
     return (
       <>
         <p>
-          This documentation will outline an efficient yet scalable peer-to-peer consensus mechanism that allows for
-          highly performant transaction validation within a decentralized network.
+          This documentation outlines an efficient and scalable peer-to-peer consensus mechanism that allows for highly
+          performant transaction validation within a decentralized network.
         </p>
         <p>
           The architecture is based on the idea that when building a distributed payment ledger, it is not the
@@ -62,43 +59,50 @@ const GuideIntroduction: FC = () => {
       lastUpdated="06 Mar 2021"
     >
       <p>
-        The system has many components, each of which plays a specific role in allowing the transfer of coins (the
-        currency of the system) securely between accounts. Future sections will discuss each of these elements in more
-        detail, but for now, we will examine a simplified network composed of the core components.
+        The system has many components, each of which plays a specific role in secure transfer of coins (the currency of
+        the system) between accounts. Other sections of our documentation discuss these elements in more detail, but for
+        now, we examine a simplified network composed of the core components. A basic glossary follows:
       </p>
 
       {renderGlossary()}
-
+      <p>All transactions on the network begin with a user account. A typical flow looks like this:</p>
       <DocImage alt="Overview Diagram" maxWidth={900} src={OverviewDiagram} />
-
+      <DocList variant="ol">
+        <li>
+          The owner of the account creates a block (a group of transactions), indicating the number of coins they would
+          like to send to each recipient
+        </li>
+        <li>The owner of the account sends this block to the node</li>
+        <li>The node forwards that block (now a node block) to the primary validator</li>
+        <li>
+          If the transaction is successfully validated, the validator appends the block onto the blockchain and account
+          balances are updated
+        </li>
+      </DocList>
       <p>
-        All transactions on the network will begin with a user account. The owner of the account will create a block (a
-        group of transactions), indicating the number of coins they would like to send to each recipient, and then send
-        that block to their node. The node will then forward the block (now a node block) to the primary validator
-        which, upon successful validation of the transaction, appends it onto the blockchain while updating account
-        balances. There are several key differences between the network structure outlined above and the traditional
-        Blockchain architecture regarding the creation of a distributed public ledger.
+        Regarding the creation of a distributed public ledger, there are several key differences between this network
+        structure and the traditional Blockchain architecture.
+      </p>
+      <DocSubHeader>Key benefits</DocSubHeader>
+      <p>
+        An inherent defect in modern Blockchain architecture is the inefficient composition of blocks. For example,
+        blocks in the Bitcoin Blockchain are composed of multiple unrelated transactions. This indicates that within
+        every block, the earliest transactions experience significant delays, while later transactions continue to
+        accumulate until the entire block gets verified. Focusing on the performance of the Blockchain and nothing else,
+        in an ideal architecture, transactions must be validated immediately after they are received, instead of waiting
+        for them to bundle along with additional unrelated transactions.
       </p>
       <p>
-        An inherent defect in modern Blockchain architecture is the inefficient composition of blocks. Blocks in the
-        Bitcoin Blockchain is composed of multiple unrelated transactions. This indicates that within any given block,
-        the earliest transactions would have experienced significant delays as later transactions continued to
-        accumulate until the entire block was verified. If one was only considering the performance of the Blockchain
-        and nothing else, they would conclude that in an ideal architecture transactions would be validated immediately
-        as they were received rather than waiting to be bundled along with additional unrelated transactions.
+        By removing unrelated transactions from blocks, we can substantially reduce the average network block size. This
+        is accomplished by constructing blocks that group related transactions only. Transactions now refer to groups of
+        transactions that must be processed together. For details, see <NavLink to="/guide/blocks">Blocks</NavLink>.
       </p>
       <p>
-        By removing the inclusion of unrelated transactions into the blocks, we are able to substantially reduce the
-        average network block size. This is accomplished by constructing blocks that group related transactions only.
-        transactions refer to a set of transactions that must be processed together. This is discussed in more detail in
-        the <NavLink to="/guide/blocks">Blocks</NavLink> section of the guide.
-      </p>
-      <p>
-        These improvements, along with others discussed throughout this documentation when implemented correctly, reduce
-        the overall energy consumption and processing power required by the network. This increased efficiency not only
-        enables transactions between accounts to be verified within seconds (or less) but allows developers to utilize
-        more popular programming languages (Python, JavaScript, etc.) towards expediting early development while
-        encouraging rapid growth and innovation.
+        When implemented correctly, these improvements, along with others discussed throughout this documentation,
+        reduce the overall energy consumption and processing power required by the network. This increased efficiency
+        not only enables transactions between accounts to be verified within seconds (or less), but also enables
+        developers to use more popular programming languages (Python, JavaScript, etc.) towards expediting early
+        development, while encouraging rapid growth and innovation.
       </p>
     </DocContainer>
   );
