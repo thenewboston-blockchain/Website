ec8dd7271b4af4663bdd4481ced0dd72f5705463
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2021-03-09 10:54:57 +0530

    Dashboard tests (#1458)
    * tests written for dashboard layout
    
    * chore: minor changes
    
    * chore: revert package-lock.json
    
    * chore: synced package.json and package-lock.json
    
    * uninstall
    
    Co-authored-by: Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
    Co-authored-by: Justin Kim <justinkim943@gmail.com>

---
+++ src/components/DashboardLayout/DashboardLayout.test.tsx
@@ -0,0 +1,58 @@
+import React, {FC} from 'react';
+import {HelmetProvider} from 'react-helmet-async';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {render, screen, waitFor} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import DashboardLayout, {DashboardLayoutProps} from '.';
+
+const baseProps: DashboardLayoutProps = {
+  children: <p>Children</p>,
+  menuItems: <p>Test</p>,
+  pageName: 'TestPage',
+  sectionName: 'TestSection',
+};
+
+const Wrapper: FC = ({children}) => {
+  return (
+    <Router>
+      <HelmetProvider>{children}</HelmetProvider>
+    </Router>
+  );
+};
+
+describe('Dashboard Component', () => {
+  it('renders without crashing', () => {
+    render(<DashboardLayout {...baseProps} />, {wrapper: Wrapper});
+  });
+
+  test('expect to have proper page title passed in', async () => {
+    render(<DashboardLayout {...baseProps} />, {wrapper: Wrapper});
+
+    await waitFor(() => expect(document.title).toEqual(`${baseProps.sectionName} | thenewboston`));
+  });
+
+  test('expect to have breadcrumb menu rendered from dashboard', () => {
+    render(<DashboardLayout {...baseProps} />, {wrapper: Wrapper});
+
+    expect(screen.getByTestId('BreadcrumbMenu')).toHaveClass('DashboardLayout__BreadcrumbMenu');
+  });
+
+  test('expect to have proper pageName passed in', () => {
+    render(<DashboardLayout {...baseProps} />, {wrapper: Wrapper});
+
+    expect(screen.getByTestId('BreadcrumbMenu__navigation')).toHaveTextContent(baseProps.pageName);
+  });
+
+  test('expect to have proper menuItems passed in', () => {
+    render(<DashboardLayout {...baseProps} />, {wrapper: Wrapper});
+
+    expect(screen.getByTestId('DashboardLayout__left-menu_test').firstChild).toHaveTextContent('Test');
+  });
+
+  test('expect to have proper children passed in', () => {
+    render(<DashboardLayout {...baseProps} />, {wrapper: Wrapper});
+
+    expect(screen.getByTestId('DashboardLayout__main-content_test').firstChild).toHaveTextContent('Children');
+  });
+});
