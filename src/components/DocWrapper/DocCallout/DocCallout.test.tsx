14c66c79dee05056935c13dd6cb42a5aff93624d
Prince Raj Roy <princerajroy313@gmail.com>
2021-03-11 17:38:43 +0530

    Test: DocCallout (Added Further Tests)

--- src/components/DocWrapper/DocCallout/DocCallout.test.tsx
+++ src/components/DocWrapper/DocCallout/DocCallout.test.tsx
@@ -9,6 +9,8 @@ const baseProps: DocCalloutProps = {
   type: CalloutType.note,
 };
 
+const {type} = baseProps;
+
 describe('DocCallout Component', () => {
   it('renders without crashing', () => {
     render(<DocCallout {...baseProps} />);
@@ -20,19 +22,22 @@ describe('DocCallout Component', () => {
     render(<DocCallout {...baseProps} />);
 
     expect(screen.getByTestId('DocCallout')).toHaveClass('DocCallout');
+    expect(screen.getByTestId('DocCallout')).toHaveClass(`DocCallout--${type}`);
     expect(screen.getByTestId('DocCallout__header')).toHaveClass('DocCallout__header');
+    expect(screen.getByTestId('DocCallout__header')).toHaveClass(`DocCallout__header--${type}`);
   });
 
   it('renders with className passed in', () => {
     render(<DocCallout {...baseProps} />);
 
     expect(screen.getByTestId('DocCallout')).toHaveClass('Test');
+    expect(screen.getByTestId('DocCallout')).toHaveClass(`Test--${type}`);
     expect(screen.getByTestId('DocCallout__header')).toHaveClass('Test__header');
+    expect(screen.getByTestId('DocCallout__header')).toHaveClass(`Test__header--${type}`);
   });
 
   it('renders with type passed in', () => {
     render(<DocCallout {...baseProps} />);
-    const {type} = baseProps;
 
     expect(screen.getByText(type, {exact: false})).toBeTruthy();
   });
