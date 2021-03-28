e60a65e5b8977fba57967b8cfd46a662759f30d0
Bucky <buckyroberts@gmail.com>
2021-03-27 22:35:12 -0700

    Proposal details

--- src/containers/Projects/ProjectsMilestones/index.tsx
+++ src/containers/Projects/ProjectsMilestones/index.tsx
@@ -39,55 +39,30 @@ const ProjectsMilestones: FC = () => {
         number={1}
         text={
           <>
-            All the members of approved projects will receive <strong>weekly payments</strong> with pay per day of{' '}
-            <strong>2800</strong> coins.
+            Upon completion of a milestone, the project manager will submit a{' '}
+            <A href="https://github.com/thenewboston-developers/Project-Proposals/issues/new?assignees=&labels=&template=milestone-payout-request.md&title=NAME_OF_PROJECT+-+NAME_OF_MILESTONE+-+MILESTONE_NUMBER">
+              Milestone Payout Request
+            </A>{' '}
+            including valid proof of work for all objectives.
           </>
         }
       />
       <StepIndicator
         className="ProjectsMilestones__StepIndicator"
         number={2}
-        text={
-          <>
-            The project leader (or manager) has to provide a <strong>weekly report</strong> of the project’s progress
-            and each team member's weekly contribution by raising an issue to{' '}
-            <A href="https://github.com/thenewboston-developers/Activity-Reports/issues/new/choose">Activity-Reports</A>
-            repository. To know more about activity reports please go through{' '}
-            <A href="https://github.com/thenewboston-developers/Activity-Reports/blob/master/README.md">README.md</A>
-          </>
-        }
+        text="The audit team will then review the proof of work to ensure that all objectives have been met. The audit team will leave a comment if there are any questions or clarifications that are needed."
       />
       <StepIndicator
         className="ProjectsMilestones__StepIndicator"
         number={3}
         text={
           <>
-            A good <strong>Weekly Report</strong> consists of each member's tasks for that week and proof of the
-            completion of each given task. The report should also have the contribution details of the project leader.
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsMilestones__StepIndicator"
-        number={4}
-        text="Once we verify the report and progress of the project, we’ll send weekly payments to those members of the project who contributed."
-      />
-      <StepIndicator
-        className="ProjectsMilestones__StepIndicator"
-        number={5}
-        text={
-          <>
-            Failing to provide a weekly report may result in <strong>non-payment</strong>.
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsMilestones__StepIndicator"
-        number={6}
-        text={
-          <>
-            We are rewarding <strong>3000</strong> coins for <strong>Idea Only</strong> proposals after receiving 1st
-            approved weekly report from the team that is executing that project idea.
+            Once a milestone has been accepted by the audit team, payment will be sent and the project lead may then
+            complete the next{' '}
+            <A href="https://github.com/thenewboston-developers/Project-Proposals/issues/new?assignees=&labels=&template=milestone-proposal.md&title=NAME_OF_PROJECT+-+NAME_OF_MILESTONE+-+MILESTONE_NUMBER">
+              Milestone Proposal
+            </A>{' '}
+            according to their next roadmap milestone.
           </>
         }
       />
