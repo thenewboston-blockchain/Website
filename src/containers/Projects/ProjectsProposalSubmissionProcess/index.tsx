84dff1e8269309b3fcb70e4076fa0b7c2eccc9fc
Bucky <buckyroberts@gmail.com>
2021-03-27 22:24:35 -0700

    Proposal details

--- src/containers/Projects/ProjectsProposalSubmissionProcess/index.tsx
+++ src/containers/Projects/ProjectsProposalSubmissionProcess/index.tsx
@@ -1,101 +1,47 @@
 import React, {FC} from 'react';
 
-import {A, DocContainer, Label, StepIndicator} from 'components';
+import {A, DocContainer, Label} from 'components';
 
 import './ProjectsProposalSubmissionProcess.scss';
 
 const ProjectsProposalSubmissionProcess: FC = () => {
   return (
     <DocContainer className="ProjectsProposalSubmissionProcess" title="Proposal Submission Process">
-      <p>To submit a project, please follow the following steps below.</p>
-      <StepIndicator
-        className="ProjectsProposalSubmissionProcess__StepIndicator"
-        number={1}
-        text={
-          <>
-            Go to{' '}
-            <A href="https://github.com/thenewboston-developers/Project-Proposals">
-              https://github.com/thenewboston-developers/Project-Proposals
-            </A>{' '}
-            and read thoroughly README.md file.
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsProposalSubmissionProcess__StepIndicator"
-        number={2}
-        text={
-          <>
-            Raise an issue for{' '}
-            <A href="https://github.com/thenewboston-developers/Project-Proposals/issues/new?assignees=&labels=Project&template=add-project-proposal.md&title=NAME_OF_YOUR_PROJECT">
-              Project Proposal
-            </A>{' '}
-            and fill all the required details in the given format. You can also raise an{' '}
-            <A href="https://github.com/thenewboston-developers/Project-Proposals/issues/new?assignees=&labels=Idea+Only&template=idea-only-proposal.md&title=NAME_OF_YOUR_IDEA">
-              Idea Only Proposal
-            </A>{' '}
-            if you have an idea but not able to or interested in working on the project itself.
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsProposalSubmissionProcess__StepIndicator"
-        number={3}
-        text={
-          <>
-            Project proposals are labeled as <Label name="Project" color="97DEFC" /> and Idea proposals are labeled as{' '}
-            <Label name="Idea Only" color="F9EDA9" />. Idea only proposals can be taken by anyone who is willing to work
-            on it. Anyone ready to work on idea only proposal requires to comment down the{' '}
-            <strong>project milestones</strong> and the <strong>team details</strong>.
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsProposalSubmissionProcess__StepIndicator"
-        number={4}
-        text={
-          <>
-            If your project adheres to all Rules &amp; Guidelines of thenewboston, your proposal will be labeled{' '}
-            <Label name="Pending Review" color="FBCA04" />.
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsProposalSubmissionProcess__StepIndicator"
-        number={5}
-        text={
-          <>
-            If your proposal is good enough to impress thenewboston team, your proposal will be approved and your
-            proposal will be labeled as <Label name="Pending Repository" color="006B75" />
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsProposalSubmissionProcess__StepIndicator"
-        number={6}
-        text={
-          <>
-            After that, you have to comment down the <strong>GitHub repository URL</strong> of your project.
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsProposalSubmissionProcess__StepIndicator"
-        number={7}
-        text={
-          <>
-            Once done, your project will be successfully labeled as <Label name="Approved" color="0E8A16" /> and your
-            project folder with your proposal issue number and the project name will be added to the{' '}
-            <A href="https://github.com/thenewboston-developers/Project-Proposals/tree/master/projects">projects</A>{' '}
-            folder in the <strong>Project-Proposals</strong> repository. (e.g: 464-TheNewBoston)
-          </>
-        }
-      />
-      <StepIndicator
-        className="ProjectsProposalSubmissionProcess__StepIndicator"
-        number={8}
-        text="All your project details will be added to that folder."
-      />
+      <p>
+        To submit a project, fill out all project details using the{' '}
+        <A href="https://github.com/thenewboston-developers/Project-Proposals/issues/new?assignees=&labels=Project&template=project-proposal.md&title=NAME_OF_YOUR_PROJECT">
+          Project Proposal
+        </A>{' '}
+        template.
+      </p>
+
+      <h2>Status Details</h2>
+      <ul>
+        <li>
+          {' '}
+          Project proposals are labeled as <Label name="Project" color="97DEFC" /> and idea proposals are labeled as{' '}
+          <Label name="Idea Only" color="F9EDA9" />. Idea only proposals can be taken by anyone who is willing to work
+          on it.
+        </li>
+        <li>
+          {' '}
+          If your project adheres to all Rules &amp; Guidelines of thenewboston, your proposal will be labeled{' '}
+          <Label name="Pending Review" color="FBCA04" />.
+        </li>
+        <li>
+          {' '}
+          If accepted, your proposal will be labeled as <Label name="Pending Repository" color="006B75" />. You must
+          then leave a comment noting the <strong>GitHub repository URL</strong> of your project.
+        </li>
+        <li>
+          {' '}
+          Once the above steps have been completed your project will be labeled as{' '}
+          <Label name="Approved" color="0E8A16" /> and your project folder with your proposal issue number and the
+          project name will be added to the{' '}
+          <A href="https://github.com/thenewboston-developers/Project-Proposals/tree/master/projects">projects</A>{' '}
+          folder in the <strong>Project-Proposals</strong> repository. (e.g: 464-TheNewBoston)
+        </li>
+      </ul>
     </DocContainer>
   );
 };
