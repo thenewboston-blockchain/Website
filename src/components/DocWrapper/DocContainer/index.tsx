a6cab54f5057ca5d84e6a0589b7fcac2621342cb
Bucky <buckyroberts@gmail.com>
2021-04-22 12:36:02 -0700

    Doc restructuring

--- src/components/DocWrapper/DocContainer/index.tsx
+++ src/components/DocWrapper/DocContainer/index.tsx
@@ -9,56 +9,24 @@ import './DocContainer.scss';
 export interface DocContainerProps {
   className?: string;
   id?: string;
-  introSection?: ReactNode;
-  introTitle?: ReactNode;
   lastUpdated?: string;
   title: ReactNode;
 }
 
-const DocContainer: FC<DocContainerProps> = ({
-  children,
-  className,
-  id,
-  introSection,
-  introTitle,
-  lastUpdated,
-  title,
-}) => {
+const DocContainer: FC<DocContainerProps> = ({children, className, id, lastUpdated, title}) => {
   return (
     <section className={clsx('DocContainer', className)} data-testid="DocContainer">
-      {introTitle ? (
-        <div
-          className={clsx('DocContainer__wrapper', {
-            ...bemify(className, '__wrapper'),
-          })}
-          data-testid="DocContainer__wrapper"
-        >
-          <h1
-            className={clsx('DocContainer__page-title', {
-              ...bemify(className, '__page-title'),
-            })}
-            data-testid="DocContainer__page-title"
-          >
-            {introTitle}
-          </h1>
-          {Boolean(lastUpdated) && <span className="DocContainer__last-updated">Updated on {lastUpdated}</span>}
-        </div>
-      ) : null}
-      {introSection ? (
-        <>
-          {introSection}
-          <div className={clsx('DocContainer__divider')} />
-        </>
-      ) : null}
       <div
         className={clsx('DocContainer__wrapper', {
           ...bemify(className, '__wrapper'),
         })}
+        data-testid="DocContainer__wrapper"
       >
         <h1
           className={clsx('DocContainer__page-title', {
             ...bemify(className, '__page-title'),
           })}
+          data-testid="DocContainer__page-title"
           id={id}
         >
           {title}
