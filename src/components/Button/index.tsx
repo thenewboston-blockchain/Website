1408c45bab157e14915b98b15dd91e740bc8aa33
jamessspanggg <jamespangmunwai@gmail.com>
2021-04-24 23:27:38 +0800

    Add iconLeft support in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,15 +6,17 @@ import './Button.scss';
 
 type Props = {
   className?: string;
+  iconLeft?: React.ReactNode;
   iconRight?: React.ReactNode;
   onClick: (event: React.SyntheticEvent<HTMLButtonElement>) => void;
   rounded?: boolean;
   type?: 'primary' | 'outlined' | 'empty';
 };
 
-const Button: FC<Props> = ({children, className, iconRight, onClick, rounded = false, type = 'primary'}) => {
+const Button: FC<Props> = ({children, className, iconLeft, iconRight, onClick, rounded = false, type = 'primary'}) => {
   return (
     <button className={clsx('Button', `Button__${type}`, {'Button--rounded': rounded}, className)} onClick={onClick}>
+      {iconLeft && <div className="Button__icon-left">{iconLeft}</div>}
       {children}
       {iconRight && <div className="Button__icon-right">{iconRight}</div>}
     </button>
