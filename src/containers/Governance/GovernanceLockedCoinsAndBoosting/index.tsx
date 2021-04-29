f3740219a4f223293bc95fd71ddd2adfe3b51339
Bucky Roberts <buckyroberts@gmail.com>
2021-04-28 17:52:44 -0700

    Doc tweaks (#1768)

--- src/containers/Governance/GovernanceLockedCoinsAndBoosting/index.tsx
+++ src/containers/Governance/GovernanceLockedCoinsAndBoosting/index.tsx
@@ -9,21 +9,21 @@ const GovernanceLockedCoinsAndBoosting: FC = () => {
     <DocContainer
       className="GovernanceLockedCoinsAndBoosting"
       title="Locked Coins & Boosting"
-      lastUpdated="07 Mar 2021"
+      lastUpdated="28 Apr 2021"
     >
       <p>
-        Registered users have the ability to lock any amount of coins that they own. By locking coins, the network will
-        place a freeze on those coins for a set amount of time to prevent them from being withdrawn. This reduces the
-        amount of coins in circulation while increasing their stability and value.
+        Users have the ability to lock any amount of coins that they own. By locking coins, the network will place a
+        freeze on those coins for a set amount of time to prevent them from being withdrawn. This reduces the amount of
+        coins in circulation while increasing their stability and value.
       </p>
-      <p>In return for locked coins registered users will receive:</p>
+      <p>In return for locked coins users will receive:</p>
       <DocList variant="ul">
         <li>Points</li>
         <li>The ability to boost a node of their choosing</li>
       </DocList>
       <p>
-        Points are used to perform various actions on the network. Certain actions are available to all registered users
-        while other actions are restricted to governors only. Unlike coins, points refill over time. The refill rate is
+        Points are used to perform various actions on the network. Certain actions are available to all users while
+        other actions are restricted to governors only. Unlike coins, points refill over time. The refill rate is
         proportional to the amount of coins locked.
       </p>
       <DocImage alt="node boosting" maxWidth={540} src={NodeBoosting} />
