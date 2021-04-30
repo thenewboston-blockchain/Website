247f433385a7ef718afc60a3c9d5636d3d119c39
jamessspanggg <jamespangmunwai@gmail.com>
2021-04-30 23:35:09 +0800

    ! Modify openings page to have application url specific to the role

--- src/types/openings.ts
+++ src/types/openings.ts
@@ -6,6 +6,7 @@ export interface Opening {
   project?: ProjectProposal;
   responsibilities: string[];
   technologyRequirements: string[];
+  applicationUrl: string;
 }
 
 export enum OpeningCategory {
