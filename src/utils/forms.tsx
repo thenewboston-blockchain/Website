816796c05b045af13681e33d18146c842ed8e857
jamessspanggg <jamespangmunwai@gmail.com>
2021-05-10 13:32:41 +0800

    Update input error message UI

--- src/utils/forms.tsx
+++ src/utils/forms.tsx
@@ -2,12 +2,21 @@ import React, {ReactNode} from 'react';
 import clsx from 'clsx';
 import {ErrorMessage} from 'formik';
 import {bemify} from '@thenewboston/utils';
+import {Icon, IconType} from '@thenewboston/ui';
 
 import RequiredAsterisk from 'components/RequiredAsterisk';
 
 export const renderFormError = (name: string, classNames: string | undefined, hideErrorText = false): ReactNode => (
   <span className={clsx('FormFieldComponent__error-message', {...bemify(classNames, '__error-message')})}>
-    {hideErrorText ? null : <ErrorMessage name={name} />}
+    {hideErrorText ? null : (
+      <ErrorMessage name={name}>
+        {(message) => (
+          <div style={{alignItems: 'center', display: 'flex'}}>
+            <Icon icon={IconType.alert} color="#ED5F74" size={12} totalSize={12} /> {message}
+          </div>
+        )}
+      </ErrorMessage>
+    )}
   </span>
 );
 
