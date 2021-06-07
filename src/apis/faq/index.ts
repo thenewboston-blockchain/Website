215dca4df676e6af8bdfe3516f9756a05855c702
Justin Kim <justinkim943@gmail.com>
2021-06-07 11:40:08 -0500

    add a TODO for sendFeedbac

--- src/apis/faq/index.ts
+++ src/apis/faq/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {standardHeaders} from 'utils/requests';
 
+// TODO: Update with BE
 export async function sendFeedback(name: string, email: string, response: string) {
   return axios.post(
     `https://v1.nocodeapi.com/jamessspanggg/google_sheets/XKBFrUzDbyXOOFNR?tabId=Sheet1&api_key=${process.env.REACT_APP_GOOGLE_SHEET_FAQ_API_KEY}`,
