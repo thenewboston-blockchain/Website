9708f566e8ef9006e414a8098dfd1c400565d693
Justin Kim <justinkim943@gmail.com>
2021-06-09 21:06:13 -0500

    BE connected to Feedback

--- src/apis/faq/index.ts
+++ src/apis/faq/index.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import {standardHeaders} from 'utils/requests';
 
-// TODO: Update with BE
-export async function sendFeedback(name: string, email: string, response: string) {
-  return axios.post(
-    `https://v1.nocodeapi.com/jamessspanggg/google_sheets/XKBFrUzDbyXOOFNR?tabId=Sheet1&api_key=${process.env.REACT_APP_GOOGLE_SHEET_FAQ_API_KEY}`,
-    [[name, email, response]],
-    standardHeaders(),
-  );
+interface FeedbackData {
+  email: string;
+  message: string;
+  name: string;
+}
+
+export async function sendFeedback(data: FeedbackData) {
+  return axios.post(`${process.env.REACT_APP_BACKEND_API}/feedback`, data, standardHeaders());
 }
--- src/containers/Faq/Feedback/index.tsx
+++ src/containers/Faq/Feedback/index.tsx
@@ -8,23 +8,23 @@ import yup from 'utils/yup';
 import './Feedback.scss';
 
 const initialValues = {
-  content: '',
-  emailAddress: '',
+  email: '',
+  message: '',
   name: '',
 };
 
 const validationSchema = yup.object().shape({
-  content: yup.string().required('This field is required'),
-  emailAddress: yup.string().email('Please enter a valid email format').required('This field is required'),
+  email: yup.string().email('Please enter a valid email format').required('This field is required'),
+  message: yup.string().required('This field is required'),
   name: yup.string().required('This field is required'),
 });
 
 type FormValues = typeof initialValues;
 
 const Feedback: FC = () => {
-  const handleSubmit = async ({content, emailAddress, name}: FormValues) => {
+  const handleSubmit = async (values: FormValues) => {
     try {
-      await sendFeedback(name, emailAddress, content);
+      await sendFeedback(values);
       displayToast('Success! Thank you for your feedback', 'success');
     } catch (err) {
       displayErrorToast('Sorry, there was an error with your submission. Please try again later.');
@@ -48,13 +48,13 @@ const Feedback: FC = () => {
             <>
               <div className="Feedback__form-left">
                 <FormInput className="Feedback__form-input" placeholder="Name" name="name" />
-                <FormInput className="Feedback__form-input" placeholder="Email Address" name="emailAddress" />
+                <FormInput className="Feedback__form-input" placeholder="Email Address" name="email" />
               </div>
               <div className="Feedback__form-right">
                 <FormTextArea
                   className="Feedback__form-text-area"
                   placeholder="What do you want to talk about?"
-                  name="content"
+                  name="message"
                 />
                 <FormButton className="Feedback__form-button" disabled={!isValid} type="submit">
                   Submit
