58410a1ad0f958d13794507273ffd4061b9b2d3f
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-06-29 10:49:30 +0530

    feat: add pinterest account

--- src/utils/social-media.ts
+++ src/utils/social-media.ts
@@ -6,6 +6,7 @@ export const socialMediaUrls = {
   [SocialMedia.github]: 'https://github.com/thenewboston-developers',
   [SocialMedia.instagram]: 'https://www.instagram.com/thenewbostoncoin/',
   [SocialMedia.linkedin]: 'https://www.linkedin.com/company/thenewbostoncoin/',
+  [SocialMedia.pinterest]: 'https://www.pinterest.com/thenewbostoncoin/',
   [SocialMedia.reddit]: 'https://www.reddit.com/r/thenewboston/',
   [SocialMedia.twitch]: 'https://twitch.tv/thenewboston/profile',
   [SocialMedia.twitter]: 'https://twitter.com/thenewboston_og',
@@ -18,6 +19,7 @@ export const socialMediaHandles = {
   [SocialMedia.github]: 'thenewboston-developers',
   [SocialMedia.instagram]: '@thenewbostoncoin',
   [SocialMedia.linkedin]: 'thenewbostoncoin',
+  [SocialMedia.pinterest]: 'thenewbostoncoin',
   [SocialMedia.reddit]: 'r/thenewboston',
   [SocialMedia.twitch]: 'thenewboston',
   [SocialMedia.twitter]: '@thenewboston_og',
@@ -31,6 +33,7 @@ export const socialMediaDescriptions = {
     'You can find the official repositories for our projects on our GitHub and start contributing today.',
   [SocialMedia.instagram]: 'Stay up to date by following us on our Instagram.',
   [SocialMedia.linkedin]: 'Follow us on LinkedIn and expand your network.',
+  [SocialMedia.pinterest]: 'Follow us on Pinterest for crypto related news and information.',
   [SocialMedia.reddit]:
     'Our Reddit community is built to help others learn about computer science and other academic subjects.',
   [SocialMedia.twitch]: 'Follow us on twitch for regular livestreams.',
