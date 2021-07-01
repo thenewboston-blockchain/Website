c9c2605e630ea0157ef38aad2f9d54519ac9a9a6
jamessspanggg <jamespangmunwai@gmail.com>
2021-07-01 11:03:33 +0800

    Utilise ProgressiveImage for Hero image

--- src/containers/DeveloperPortal/DeveloperPortalHero/index.tsx
+++ src/containers/DeveloperPortal/DeveloperPortalHero/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
+import {ProgressiveImage} from 'components';
+import {useWindowDimensions} from 'hooks';
 import {Link} from 'react-router-dom';
 import Particles from '../Particles';
 import developerPortalImage from './Portal.webp';
+import developerPortalPlaceholderImage from './PortalPlaceholder.webp';
 
 import './DeveloperPortalHero.scss';
 
 // TODO: update links when ready
 export default function DeveloperPortalHero() {
+  const {width} = useWindowDimensions();
+
+  let imageSize = 558;
+  if (width < 768) {
+    imageSize = 280;
+  } else if (width < 992) {
+    imageSize = 412;
+  } else {
+    imageSize = 558;
+  }
+
   return (
     <div className="DeveloperPortalHero">
       <div className="DeveloperPortalHero__links-container">
@@ -22,7 +36,14 @@ export default function DeveloperPortalHero() {
         </Link>
       </div>
       <Particles />
-      <img alt="Developer Portal" src={developerPortalImage} className="DeveloperPortalHero__main-image" />
+      <ProgressiveImage
+        alt="Developer Portal"
+        containerClassName="DeveloperPortalHero__main-image"
+        height={imageSize}
+        placeholderSrc={developerPortalPlaceholderImage}
+        realSrc={developerPortalImage}
+        width={imageSize}
+      />
     </div>
   );
 }
