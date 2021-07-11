955636ae96f2aaf4ffa6f8a3cc8bae11630eae93
James Pang <jamespangmunwai@gmail.com>
2021-07-11 19:36:25 +0800

    [#1927] Remove hello text animation (#1932)
    * Remove hello text animation
    
    * Add static hello world text and remove unnecessary files
    
    * chore: remove AnimationState, useAnimationState and useShuffle hook
    
    Co-authored-by: Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,6 @@
-import useAnimationState from './useAnimationState';
 import useBooleanState from './useBooleanState';
 import useEventListener from './useEventListener';
-import useShuffle from './useShuffle';
 import useWindowDimensions from './useWindowDimensions';
 import useFormContext from './useFormContext';
 
-export {useAnimationState, useBooleanState, useEventListener, useShuffle, useWindowDimensions, useFormContext};
+export {useBooleanState, useEventListener, useWindowDimensions, useFormContext};
