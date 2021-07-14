247454ec9546bb5653e3096089beba54306731a4
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-07-15 00:41:52 +0530

    feat: node roles, locked coins and coins vs points

--- src/containers/LivingWhitepaper/containers/PrincipalEntities/index.tsx
+++ src/containers/LivingWhitepaper/containers/PrincipalEntities/index.tsx
@@ -6,6 +6,7 @@ import DeveloperPortalLayout from '../../components/DeveloperPortalLayout';
 import AccountsImage from '../../assets/accounts.svg';
 import KeysImage from '../../assets/keys.svg';
 import NodesImage from '../../assets/nodes.svg';
+import NodeBoostingImage from '../../assets/node-boosting.svg';
 import './PrincipalEntities.scss';
 
 const PrincipalEntities: FC = () => {
@@ -175,6 +176,161 @@ const PrincipalEntities: FC = () => {
             <span className="PrincipalEntities__section-text--highlight">Validators</span> are elected nodes that handle
             the validation of data and the generation of blocks that are added to the blockchain.
           </p>
+        </section>
+        <Divider className="PrincipalEntities__divider" />
+        <section className="PrincipalEntities__section">
+          <h2 className="PrincipalEntities__section-heading">Node Roles</h2>
+          <p className="PrincipalEntities__section-paragraph">Nodes perform one of these roles:</p>
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-paragraph--mb-8 PrincipalEntities__section-text--black">
+            Regular node
+          </p>
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-paragraph--my-0">
+            A network server that has several responsibilities, such as
+          </p>
+          <ul className="PrincipalEntities__section-list">
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--dense">
+              Checking that requests confirm to network protocols
+            </li>
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--dense">
+              Routing blocks to Primary Validator node
+            </li>
+          </ul>
+          <Divider className="PrincipalEntities__section-divider" />
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-paragraph--mb-8 PrincipalEntities__section-text--black">
+            Primary Validator (PV) node
+          </p>
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-paragraph--my-0">
+            A validator that accepts signed change requests from regular nodes and, after successful validation of each
+            request:
+          </p>
+          <ul className="PrincipalEntities__section-list">
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--dense">
+              Creates a new block
+            </li>
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--dense">
+              Adds that block to the PV’s own blockchain
+            </li>
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--dense">
+              Forwards it to the confirmation validators
+            </li>
+          </ul>
+          <Divider className="PrincipalEntities__section-divider" />
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-paragraph--mb-8 PrincipalEntities__section-text--black">
+            Confirmation Validator (CV) node
+          </p>
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-paragraph--my-0">
+            A validator that:
+          </p>
+          <ul className="PrincipalEntities__section-list">
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--dense">
+              Receives blocks from the PV
+            </li>
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--dense">
+              Starts the validation process for the PV’s results
+            </li>
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--dense">
+              If a block is valid, adds it as a new block to their own blockchain
+            </li>
+          </ul>
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-paragraph--mt-32">
+            At any point, the network has multiple regular nodes, a single PV, and a number of CVs.
+          </p>
+        </section>
+        <Divider className="PrincipalEntities__divider" />
+        <section className="PrincipalEntities__section">
+          <h2 className="PrincipalEntities__section-heading">Locked Coins and Boosts</h2>
+          <p className="PrincipalEntities__section-paragraph">
+            Users of the network can lock any amount of coins that they own. By locking coins, the network places a
+            freeze on those coins for a set amount of time to prevent them from being withdrawn. This reduces the amount
+            of coins in circulation while increasing their stability and value.
+          </p>
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-text--bold">
+            In return for locked coins, users receive:
+          </p>
+          <ul className="PrincipalEntities__section-list">
+            <li className="PrincipalEntities__section-list-item">Points</li>
+            <li className="PrincipalEntities__section-list-item">The ability to boost a node of their choice</li>
+          </ul>
+          <p className="PrincipalEntities__section-paragraph">
+            <span className="PrincipalEntities__section-text--highlight">Points</span> are used to perform various
+            actions on the network. Unlike coins, points refill over time. The refill rate is proportional to the amount
+            of coins locked.
+          </p>
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-paragraph--mb-32">
+            <span className="PrincipalEntities__section-text--highlight">Boosts</span> are locked coins that represent a
+            user’s trust in a node to act as a validator.
+          </p>
+          <p className="PrincipalEntities__section-paragraph">
+            Users on the network can endorse their trusted nodes by using the boosting system. All nodes (both regular
+            nodes and validators) can receive boosts. In this way, node owners receive rewards for serving the network
+            and its users, and for offering trusted and reliable services to them. The following example outlines how
+            users A and B lock 40 coins in total to offer 40 boosts to Node A. Similarly, user C boosts Node B by
+            locking 25 coins.
+          </p>
+          <div className="PrincipalEntities__section-image-container">
+            <img
+              alt="Node Boosting"
+              className="PrincipalEntities__section-image"
+              src={NodeBoostingImage}
+              loading="lazy"
+              width="360px"
+            />
+          </div>
+          <p className="PrincipalEntities__section-paragraph">
+            This mechanism enables a healthy competition between nodes over users’ trust, resulting in better quality of
+            service offered throughout the network.
+          </p>
+        </section>
+        <Divider className="PrincipalEntities__divider" />
+        <section className="PrincipalEntities__section">
+          <h2 className="PrincipalEntities__section-heading">Coins vs. Points</h2>
+          <p className="PrincipalEntities__section-paragraph">
+            The value of thenewboston coins will increase as the community develops more apps for our network. All apps,
+            however, require user interaction. Some common app interactions include leaving comments, sending messages,
+            changing passwords, and so on. The more frequently an app is used, the higher the bandwidth costs for server
+            owners will become.
+          </p>
+          <p className="PrincipalEntities__section-paragraph">
+            Blockchain networks typically rely on{' '}
+            <span className="PrincipalEntities__section-text--highlight">transaction fees</span> to reward server owners
+            and prevent spam. While fees solve the spam problem by making it expensive to attack or spam the network,
+            they also introduce a new challenge. As an analogy, imagine users having to pay a fee every time they send
+            an email or change their password. If users have to pay charges for every interaction they perform, they
+            will eventually stop using these apps. For a blockchain network that targets supporting app development,
+            preventing spam, and incentivizing server owners, there is a better solution.
+          </p>
+          <p className="PrincipalEntities__section-paragraph">
+            The goal of the point system is to address these challenges and solve these issues. By locking coins, users
+            receive points in return for services to the network.
+          </p>
+          <p className="PrincipalEntities__section-paragraph PrincipalEntities__section-text--bold">
+            Points fill back up over time and help solve challenges such as:
+          </p>
+          <ul className="PrincipalEntities__section-list">
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--spaced">
+              <span className="PrincipalEntities__section-text PrincipalEntities__section-text--black">
+                App Development
+              </span>{' '}
+              - Interactions like leaving comments or sending messages do not have a monetary cost (coins), rather they
+              require refillable points.
+            </li>
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--spaced">
+              <span className="PrincipalEntities__section-text PrincipalEntities__section-text--black">
+                Spam Prevention
+              </span>{' '}
+              - Users have a limited number of interactions they can perform based on how many points they have, so
+              spending points for spamming users makes it an overall bad idea.
+            </li>
+            <li className="PrincipalEntities__section-list-item PrincipalEntities__section-list-item--spaced">
+              <span className="PrincipalEntities__section-text PrincipalEntities__section-text--black">
+                Incentives to Server Owners
+              </span>{' '}
+              - Server owners have no direct financial incentive. However, because users can only gain points by locking
+              up their coins, when server owners receive points for their services, this increases the value of all
+              coins by reducing the overall available supply. Receiving more points leads to more locked coins and less
+              available coin supply.
+            </li>
+          </ul>
         </section>
         <Divider className="PrincipalEntities__divider" />
       </div>
