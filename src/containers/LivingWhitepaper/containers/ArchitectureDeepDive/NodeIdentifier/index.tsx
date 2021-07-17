a313dedbdde4fd1cf2a063c85d3a778bcb9c5026
jamessspanggg <jamespangmunwai@gmail.com>
2021-07-17 16:33:01 +0800

    Add temporary fix for overflowing code snippet

--- src/containers/LivingWhitepaper/containers/ArchitectureDeepDive/NodeIdentifier/index.tsx
+++ src/containers/LivingWhitepaper/containers/ArchitectureDeepDive/NodeIdentifier/index.tsx
@@ -33,7 +33,7 @@ const NodeIdentifier = () => {
     "protocol": "http"
   },
   "node_identifier": "d5356888dc9303e44ce52b1e06c3165a7759b9df1e6a6dfbd33ee1c3df1ab4d1",
-  "signature": "3c88665e123e7e25a8b9d9592f3269ab4efc4bcba989a103a898e2625933261b1cccdaf2f52eca9c58d2bf033968ab6b702089bca8fc6e0c80b3b002a5e05b03"
+  "signature": "3c88665e123e7e25a8b9d9592f3269ab4efc4bcba98......"
 }`}
         language={SnippetLang.json}
       />
