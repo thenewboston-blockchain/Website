06a4ccb796c44510f72b9fe3b11a45110d3d4493
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-07-18 03:08:08 +0530

    feat: add all folds in the page

---
+++ src/containers/LivingWhitepaper/containers/PrincipalEventsAndProcesses/Overview/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const Overview: FC = () => {
+  return (
+    <section className="PrincipalEvents__section">
+      <h2 className="PrincipalEvents__section-heading">Overview</h2>
+    </section>
+  );
+};
+
+export default Overview;
