a4e2695ace91a2ca0e0ff8dcbe5b03537c238199
jamessspanggg <jamespangmunwai@gmail.com>
2021-07-20 10:00:48 +0800

    Utilise react-measure for dynamic breadcrumb height

--- src/containers/LivingWhitepaper/components/DeveloperPortalLayout/index.tsx
+++ src/containers/LivingWhitepaper/components/DeveloperPortalLayout/index.tsx
@@ -1,6 +1,7 @@
-import React, {FC, ReactNode} from 'react';
+import React, {FC, ReactNode, useState} from 'react';
 
 import {Container, Divider, PageTitle} from 'components';
+import Measure from 'react-measure';
 import TopLinks from '../TopLinks';
 import Breadcrumb from '../Breadcrumb';
 import SideMenu from '../SideMenu';
@@ -13,20 +14,25 @@ type Props = {
 };
 
 const DeveloperPortalLayout: FC<Props> = ({children, pageName}) => {
+  const [breadcrumbHeight, setBreadcrumbHeight] = useState(0);
   return (
     <>
       <PageTitle title={pageName} />
       <TopLinks />
-      <div className="DeveloperPortalLayout__breadcrumb">
-        <Container>
-          <Breadcrumb />
-        </Container>
-        <Divider />
-      </div>
+      <Measure bounds onResize={(contentRect) => setBreadcrumbHeight(contentRect?.bounds?.height || 0)}>
+        {({measureRef}) => (
+          <div className="DeveloperPortalLayout__breadcrumb" ref={measureRef}>
+            <Container>
+              <Breadcrumb breadcrumbHeight={breadcrumbHeight} />
+            </Container>
+            <Divider />
+          </div>
+        )}
+      </Measure>
       <Container>
         <div className="DeveloperPortalLayout__main-content">
           <div className="DeveloperPortalLayout__left-content">
-            <SideMenu />
+            <SideMenu breadcrumbHeight={breadcrumbHeight} />
           </div>
           <div className="DeveloperPortalLayout__right-content">{children}</div>
         </div>
