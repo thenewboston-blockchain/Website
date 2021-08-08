08e92b4ae9faaaafde1091b3983a5664d23728d3
jamessspanggg <jamespangmunwai@gmail.com>
2021-08-08 09:59:18 +0800

    Fix sidemenu and breadcrumb

--- src/containers/DeveloperPortalProjects/components/SideMenu/index.tsx
+++ src/containers/DeveloperPortalProjects/components/SideMenu/index.tsx
@@ -2,15 +2,11 @@ import React, {FC} from 'react';
 
 import {Icon, IconType} from '@thenewboston/ui';
 import clsx from 'clsx';
+import {A} from 'components';
 import {NAVBAR_HEIGHT, LIVING_WHITEPAPER_TOP_LINKS_HEIGHT} from 'constants/offsets';
 import {Link} from 'react-scroll';
 import {useLocation, useHistory} from 'react-router';
-import {
-  PATHNAME_TO_DROPDOWN_SELECTIONS,
-  architecturePath,
-  principalEntitiesPath,
-  principalEventsPath,
-} from '../../constants';
+import {PATHNAME_TO_DROPDOWN_SELECTIONS, projectRulesPath} from '../../constants';
 
 import './SideMenu.scss';
 
@@ -22,30 +18,25 @@ const SideMenu: FC<Props> = ({breadcrumbHeight}) => {
   const {pathname} = useLocation();
   const history = useHistory();
 
-  const isPrincipalEntitiesSelected = pathname.includes(principalEntitiesPath);
-  const isPrincipalEventsSelected = pathname.includes(principalEventsPath);
-  const isArchitectureSelected = pathname.includes(architecturePath);
+  const isProjectRulesSelected = pathname.includes(projectRulesPath);
 
   return (
     <div className="SideMenu">
       <div className="SideMenu__section">
         <button
-          className={clsx(
-            'SideMenu__section-header',
-            isPrincipalEntitiesSelected && 'SideMenu__section-header--active',
-          )}
-          onClick={() => history.push(principalEntitiesPath)}
+          className={clsx('SideMenu__section-header', isProjectRulesSelected && 'SideMenu__section-header--active')}
+          onClick={() => history.push(projectRulesPath)}
         >
-          <div>Principle Entities on the Network</div>
+          <div>RULES & GUIDELINES</div>
           <Icon
             className="SideMenu__toggle-icon"
-            icon={isPrincipalEntitiesSelected ? IconType.chevronUp : IconType.chevronDown}
+            icon={isProjectRulesSelected ? IconType.chevronUp : IconType.chevronDown}
             size={20}
             totalSize={20}
           />
         </button>
-        {isPrincipalEntitiesSelected &&
-          PATHNAME_TO_DROPDOWN_SELECTIONS['principal-entities'].map((selection) => {
+        {isProjectRulesSelected &&
+          PATHNAME_TO_DROPDOWN_SELECTIONS.rules.map((selection) => {
             const selectionHash = selection.url.slice(selection.url.indexOf('#') + 1);
             return (
               <Link
@@ -64,73 +55,13 @@ const SideMenu: FC<Props> = ({breadcrumbHeight}) => {
             );
           })}
       </div>
-
       <div className="SideMenu__section">
-        <button
-          className={clsx('SideMenu__section-header', isPrincipalEventsSelected && 'SideMenu__section-header--active')}
-          onClick={() => history.push(principalEventsPath)}
+        <A
+          className="SideMenu__section-header"
+          href="https://github.com/thenewboston-developers/Projects/issues/new?assignees=&labels=Project&template=project-proposal.md&title=NAME_OF_YOUR_PROJECT"
         >
-          <div>Principle Events and Processes on the Network</div>
-          <Icon
-            className="SideMenu__toggle-icon"
-            icon={isPrincipalEventsSelected ? IconType.chevronUp : IconType.chevronDown}
-            size={20}
-            totalSize={20}
-          />
-        </button>
-        {isPrincipalEventsSelected &&
-          PATHNAME_TO_DROPDOWN_SELECTIONS['principal-events'].map((selection) => {
-            const selectionHash = selection.url.slice(selection.url.indexOf('#') + 1);
-            return (
-              <Link
-                activeClass="SideMenu__link--active"
-                className={clsx('SideMenu__link')}
-                hashSpy
-                ignoreCancelEvents
-                key={selection.url}
-                offset={-(NAVBAR_HEIGHT + LIVING_WHITEPAPER_TOP_LINKS_HEIGHT + breadcrumbHeight)}
-                smooth
-                spy
-                to={selectionHash}
-              >
-                {selection.title}
-              </Link>
-            );
-          })}
-      </div>
-
-      <div className="SideMenu__section">
-        <button
-          className={clsx('SideMenu__section-header', isArchitectureSelected && 'SideMenu__section-header--active')}
-          onClick={() => history.push(architecturePath)}
-        >
-          <div>Architecture - Deep Dive</div>
-          <Icon
-            className="SideMenu__toggle-icon"
-            icon={isArchitectureSelected ? IconType.chevronUp : IconType.chevronDown}
-            size={20}
-            totalSize={20}
-          />
-        </button>
-        {isArchitectureSelected &&
-          PATHNAME_TO_DROPDOWN_SELECTIONS.architecture.map((selection) => {
-            const selectionHash = selection.url.slice(selection.url.indexOf('#') + 1);
-            return (
-              <Link
-                activeClass="SideMenu__link--active"
-                className={clsx('SideMenu__link')}
-                hashSpy
-                ignoreCancelEvents
-                key={selection.url}
-                offset={-(NAVBAR_HEIGHT + LIVING_WHITEPAPER_TOP_LINKS_HEIGHT + breadcrumbHeight)}
-                smooth
-                spy
-                to={selectionHash}
-              >
-                {selection.title}
-              </Link>
-            );
-          })}
+          PROPOSE A PROJECT
+        </A>
       </div>
     </div>
   );
