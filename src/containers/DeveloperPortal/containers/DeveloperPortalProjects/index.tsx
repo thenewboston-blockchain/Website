656a222e23cfa4a1341879e5d46ac8291d73b2d3
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-08-16 22:32:36 +0530

    refactor: add loading and error states

--- src/containers/DeveloperPortal/containers/DeveloperPortalProjects/index.tsx
+++ src/containers/DeveloperPortal/containers/DeveloperPortalProjects/index.tsx
@@ -1,23 +1,55 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {FC, ReactNode, useEffect, useState} from 'react';
+import {useHistory} from 'react-router';
 
 import {api as projectsApi} from 'apis/projects';
-import {Button} from 'components';
-import {useHistory} from 'react-router';
+import {Button, Loader} from 'components';
 import {Project} from 'types/projects';
-import DeveloperPortalLayout from './components/DeveloperPortalLayout';
 import ProjectsIcon from './assets/ProjectsIcon.webp';
+import DeveloperPortalLayout from './components/DeveloperPortalLayout';
+import {approvedProjectsPath} from './constants';
 
 import './Projects.scss';
 
 const Projects: FC = () => {
   const history = useHistory();
+
   const [featuredProjects, setFeaturedProjects] = useState<Project[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
 
   useEffect(() => {
-    projectsApi.getProjects().then((projects) => {
-      setFeaturedProjects(projects.filter((project) => project.is_featured));
-    });
+    try {
+      (async () => {
+        const allProjects = await projectsApi.getProjects();
+        setFeaturedProjects(allProjects.filter((project) => project.is_featured));
+      })();
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
   }, []);
+
+  const renderFeaturedProjects = (): ReactNode => {
+    if (isLoading) return <Loader className="Projects__showcase-projects-loader" />;
+    if (error) return <div className="Projects__showcase-projects-error">{error}</div>;
+    return (
+      <div className="Projects__showcase-projects">
+        {featuredProjects.map((project) => (
+          <div
+            className="Projects__showcase-projects-tile"
+            key={project.pk}
+            onClick={() => history.push(`${approvedProjectsPath}/${project.pk}`)}
+            role="button"
+            tabIndex={0}
+          >
+            <h3 className="Projects__showcase-projects-tile-title">{project.title}</h3>
+            <p className="Projects__showcase-projects-tile-description">{project.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   return (
     <DeveloperPortalLayout pageName="Projects">
@@ -53,22 +85,7 @@ const Projects: FC = () => {
       </div>
       <div className="Projects__showcase">
         <div className="Projects__showcase-title">Featured Projects</div>
-        <div className="Projects__showcase-projects">
-          {featuredProjects.map((project) => {
-            return (
-              <div
-                className="Projects__showcase-projects-tile"
-                key={project.pk}
-                onClick={() => history.push(`/developer/projects/approved-projects/${project.pk}`)}
-                role="button"
-                tabIndex={0}
-              >
-                <h3 className="Projects__showcase-projects-tile-title">{project.title}</h3>
-                <p className="Projects__showcase-projects-tile-description">{project.description}</p>
-              </div>
-            );
-          })}
-        </div>
+        {renderFeaturedProjects()}
       </div>
     </DeveloperPortalLayout>
   );
