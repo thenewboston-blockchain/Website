94210d973960f4c64d8e359b798891f512d4d1f4
James Pang <jamespangmunwai@gmail.com>
2021-08-17 08:17:02 +0800

    Add featured projects (#2020)
    * Add featured projects
    
    * refactor: add loading and error states
    
    * fix: title line count
    
    Co-authored-by: Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>

--- src/containers/DeveloperPortal/containers/DeveloperPortalProjects/index.tsx
+++ src/containers/DeveloperPortal/containers/DeveloperPortalProjects/index.tsx
@@ -1,14 +1,55 @@
-import React, {FC} from 'react';
-
-import {Button} from 'components';
+import React, {FC, ReactNode, useEffect, useState} from 'react';
 import {useHistory} from 'react-router';
-import DeveloperPortalLayout from './components/DeveloperPortalLayout';
+
+import {api as projectsApi} from 'apis/projects';
+import {Button, Loader} from 'components';
+import {Project} from 'types/projects';
 import ProjectsIcon from './assets/ProjectsIcon.webp';
+import DeveloperPortalLayout from './components/DeveloperPortalLayout';
+import {approvedProjectsPath} from './constants';
 
 import './Projects.scss';
 
 const Projects: FC = () => {
   const history = useHistory();
+
+  const [featuredProjects, setFeaturedProjects] = useState<Project[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const allProjects = await projectsApi.getProjects();
+        setFeaturedProjects(allProjects.filter((project) => project.is_featured));
+      })();
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const renderFeaturedProjects = (): ReactNode => {
+    if (isLoading) return <Loader className="Projects__showcase-projects-loader" />;
+    if (error) return <div className="Projects__showcase-projects-error">{error}</div>;
+    return (
+      <div className="Projects__showcase-projects">
+        {featuredProjects.map((project) => (
+          <div
+            className="Projects__showcase-projects-tile"
+            key={project.pk}
+            onClick={() => history.push(`${approvedProjectsPath}/${project.pk}`)}
+            role="button"
+            tabIndex={0}
+          >
+            <h3 className="Projects__showcase-projects-tile-title">{project.title}</h3>
+            <p className="Projects__showcase-projects-tile-description">{project.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   return (
     <DeveloperPortalLayout pageName="Projects">
@@ -44,15 +85,7 @@ const Projects: FC = () => {
       </div>
       <div className="Projects__showcase">
         <div className="Projects__showcase-title">Featured Projects</div>
-        <div className="Projects__showcase-projects">
-          <div className="Projects__showcase-projects-tile">
-            <h3 className="Projects__showcase-projects-tile-title">Blockchain Explorer</h3>
-            <p className="Projects__showcase-projects-tile-description">
-              A blockchain browser for everyone. Functional Requirements of Blockchain Explorer: - The system must
-              track* all transactions in real....
-            </p>
-          </div>
-        </div>
+        {renderFeaturedProjects()}
       </div>
     </DeveloperPortalLayout>
   );
