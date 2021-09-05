643b24fe0c782fedd3fd150a8f689f8eef59f915
James Pang <jamespangmunwai@gmail.com>
2021-09-05 18:56:01 +0800

    Convert FaqDropdownCard and FlatNavLinks to styled-components (#2041)
    * Convert FaqDropdownCard and FlatNavLinks to styled-components
    
    * Fix breadcrumb issue

--- src/components/FaqDropdownCard/index.tsx
+++ src/components/FaqDropdownCard/index.tsx
@@ -1,12 +1,10 @@
 import React, {ReactNode, useEffect} from 'react';
-import {Link, useLocation} from 'react-router-dom';
-import clsx from 'clsx';
+import {useLocation} from 'react-router-dom';
 import {Icon, IconType} from '@thenewboston/ui';
 
 import {useBooleanState} from 'hooks';
-import {HashLink} from 'components';
 import {SFC} from 'types/generic';
-import './FaqDropdownCard.scss';
+import * as S from './Styles';
 
 type Props = {
   answer: ReactNode;
@@ -25,23 +23,23 @@ const FaqDropdownCard: SFC<Props> = ({answer, className, id, question}) => {
   }, [hash, id, pathname, setExpandedAsTrue]);
 
   return (
-    <div className={clsx('FaqDropdownCard', className)}>
-      <div className="FaqDropdownCard__anchor" id={id} />
-      <div className="FaqDropdownCard__dropdown-container">
-        <div className="FaqDropdownCard__left-container">
-          <Link className="FaqDropdownCard__question" to={`${pathname}#${id}`} onClick={toggleExpanded}>
+    <S.Container className={className}>
+      <S.Anchor className="FaqDropdownCard__anchor" id={id} />
+      <S.DropdownContainer>
+        <S.LeftContainer>
+          <S.Question to={`${pathname}#${id}`} onClick={toggleExpanded}>
             {question}
-          </Link>
-          <HashLink className="FaqDropdownCard__HashLink" id={id} />
-        </div>
+          </S.Question>
+          <S.HashLink className="FaqDropdownCard__HashLink" id={id} />
+        </S.LeftContainer>
         <Icon
           className="FaqDropdownCard__toggle-icon"
           icon={expanded ? IconType.chevronUp : IconType.chevronDown}
           onClick={toggleExpanded}
         />
-      </div>
-      <div className={clsx('FaqDropdownCard__answer', {'FaqDropdownCard__answer--expanded': expanded})}>{answer}</div>
-    </div>
+      </S.DropdownContainer>
+      {expanded && <S.ExpandedAnswer>{answer}</S.ExpandedAnswer>}
+    </S.Container>
   );
 };
 
