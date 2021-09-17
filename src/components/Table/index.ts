d688a0f551fd29aa56766af6f4358b1915e358e6
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-09-18 04:11:58 +0530

    chore: remove unused components (#2053)

--- src/components/Table/index.ts
+++
@@ -1,5 +0,0 @@
-import TableBorderGrid from './TableBorderGrid';
-import TableParams from './TableParams';
-import TableVertical from './TableVertical';
-
-export {TableBorderGrid, TableParams, TableVertical};
