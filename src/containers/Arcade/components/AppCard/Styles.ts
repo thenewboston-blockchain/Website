b622011a81eeced9f2810d82061a030859350c39
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-09-29 00:54:06 +0530

    refactor: apps list

--- src/containers/Arcade/components/AppCard/Styles.ts
+++ src/containers/Arcade/components/AppCard/Styles.ts
@@ -1,72 +1,43 @@
 import styled from 'styled-components';
 
 import colors from 'styles/colors';
-import {h3, b2, b1} from 'styles/fonts';
-import {fontWeightBold} from 'styles/fonts/fontWeights';
+import {h3, b2, h2, b1} from 'styles/fonts';
+
+type ThumbnailsProps = {
+  size: number;
+};
 
 export const Container = styled.div`
   cursor: pointer;
-  height: 100%;
   position: relative;
-  transition: transform 0.2s;
-  width: 100%;
-
-  &:hover {
-    transform: scale(1.03);
-  }
 `;
 
-export const Banner = styled.img`
-  border-radius: 8px;
-  height: 195px;
+export const Thumbnail = styled.img<ThumbnailsProps>`
+  height: ${(props) => props.size}px;
   object-fit: cover;
-  width: 100%;
+  width: ${(props) => props.size}px;
 `;
 
 export const BottomContainer = styled.div`
-  display: flex;
   margin-top: 24px;
 `;
 
-export const DetailsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-export const AppTitleDescriptionContainer = styled.div`
-  min-height: 114px;
-`;
-
 export const AppTitle = styled.h3`
-  ${h3.medium};
-  color: ${colors.palette.gray['800']};
+  ${h3.medium}
+  color: ${colors.palette.neutral['900']};
   margin-bottom: 8px;
-`;
 
-export const AppDescription = styled.p`
-  ${b2.regular};
-  -webkit-box-orient: vertical;
-  color: ${colors.palette.gray['500']};
-  display: -webkit-box;
-  -webkit-line-clamp: 4; /* max number of lines to show */
-  margin: 0;
-  margin-bottom: 16px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
-export const AppWebsite = styled.div`
-  ${b1.regular};
-  align-items: center;
-  cursor: pointer;
-  display: flex;
-
-  &:hover {
-    color: ${colors.palette.blue['500']};
-    ${fontWeightBold};
+  @media (max-width: 768px) {
+    ${h2.medium}
   }
 `;
 
-export const AppWebsiteText = styled.span`
-  margin-right: 8px;
+export const AppCategory = styled.p`
+  ${b2.regular}
+  color: ${colors.palette.neutral['500']};
+  margin: 0;
+
+  @media (max-width: 768px) {
+    ${b1.regular}
+  }
 `;
--- src/containers/Arcade/components/AppCard/index.tsx
+++ src/containers/Arcade/components/AppCard/index.tsx
@@ -1,53 +1,29 @@
-import React, {FC, useState} from 'react';
+import React, {FC} from 'react';
 import {useHistory} from 'react-router';
-import {Icon, IconType} from '@thenewboston/ui';
+
 import * as S from './Styles';
 
 type Props = {
-  bannerUrl: string;
-  description: string;
+  thumbnailUrl: string;
   id: string;
   title: string;
-  websiteUrl: string;
+  category: string;
+  thumbnailSize?: number;
 };
 
-const AppCard: FC<Props> = ({bannerUrl, description, id, title, websiteUrl}) => {
-  const [isHovered, setIsHovered] = useState(false);
+const AppCard: FC<Props> = ({thumbnailUrl, id, title, category, thumbnailSize = 280}) => {
   const history = useHistory();
 
   const handleClickCard = (): void => {
     history.push(`/arcade/${id}`);
   };
 
-  const handleVisitWebsite = (event: React.MouseEvent<HTMLDivElement>): void => {
-    event.stopPropagation(); // prevent from calling the click event at parent, i.e. clicking of card
-    window.open(websiteUrl, '_blank', 'noopener noreferrer');
-  };
-
   return (
     <S.Container role="button" onClick={handleClickCard} tabIndex={0}>
-      <S.Banner alt={title} loading="lazy" src={bannerUrl} />
+      <S.Thumbnail alt={title} loading="lazy" src={thumbnailUrl} size={thumbnailSize} />
       <S.BottomContainer>
-        <S.DetailsContainer>
-          <S.AppTitleDescriptionContainer>
-            <S.AppTitle>{title}</S.AppTitle>
-            <S.AppDescription>{description}</S.AppDescription>
-          </S.AppTitleDescriptionContainer>
-          <S.AppWebsite
-            role="button"
-            onClick={handleVisitWebsite}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            tabIndex={0}
-          >
-            <S.AppWebsiteText>{isHovered ? websiteUrl : 'Website'}</S.AppWebsiteText>
-            <Icon
-              icon={isHovered ? IconType.arrowRight : IconType.chevronRight}
-              size={isHovered ? 20 : 16}
-              totalSize={isHovered ? 20 : 16}
-            />
-          </S.AppWebsite>
-        </S.DetailsContainer>
+        <S.AppTitle>{title}</S.AppTitle>
+        <S.AppCategory>{category}</S.AppCategory>
       </S.BottomContainer>
     </S.Container>
   );
