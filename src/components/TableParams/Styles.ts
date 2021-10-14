727f8a4e3273e2fa0e0f090050b4f4d4bb71764e
Axuby <azubuinesamuel@gmail.com>
2021-10-14 13:26:12 +0100

    styles file setup

--- src/components/TableParams/Styles.ts
+++ src/components/TableParams/Styles.ts
@@ -1,17 +1,15 @@
 import styled from 'styled-components';
 import colors from 'styles/colors';
-import type {Shape} from '.';
+// import type {Shape} from '.';
 interface TableParamsProps {
   bordered?: boolean;
   clickable?: boolean;
-  shape?: Shape;
-  
+  // shape?: Shape;
 }
-
 
 export const TableParams = styled.div<TableParamsProps>`
   border: 1px solid #e6e6e6;
   margin: 0 0 20px;
   max-width: 100%;
   overflow-x: auto;
-`
+`;
