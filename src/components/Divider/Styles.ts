85ecfb7029566c6e6fca1d55891ec347ab1a1ce4
jamessspanggg <jamespangmunwai@gmail.com>
2021-10-16 10:44:09 +0800

    Add HomeSpeed section

--- src/components/Divider/Styles.ts
+++ src/components/Divider/Styles.ts
@@ -1,7 +1,16 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import colors from 'styles/colors';
 
-export const Container = styled.div`
-  border-bottom: 1px solid ${colors.palette.gray['075']};
-  width: 100%;
+type Props = {
+  type: 'vertical' | 'horizontal';
+};
+
+export const Container = styled.div<Props>`
+  ${(props) =>
+    props.type === 'horizontal'
+      ? css`
+          border-bottom: 1px solid ${colors.palette.gray['075']};
+          width: 100%;
+        `
+      : `border-right: 1px solid ${colors.palette.gray['075']};`}
 `;
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -2,12 +2,14 @@ import React, {FC} from 'react';
 
 import {PageTitle} from 'components';
 import HomeHero from './HomeHero';
+import HomeSpeed from './HomeSpeed';
 import DownloadWallet from './DownloadWallet';
 
 const Home: FC = () => (
   <>
     <PageTitle title="Home" />
     <HomeHero />
+    <HomeSpeed />
     <DownloadWallet />
   </>
 );
