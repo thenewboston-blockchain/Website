734e800964b30c5b6fefa3cc942b9448a42ddbcb
Romas <38385490+Creatinas@users.noreply.github.com>
2021-11-14 08:03:08 +0200

    [#2077] Converted DashboardLayout and Layout components from scss to styled-components (#2151)
    Co-authored-by: Romas Sirutavicius <romas.sirutavicius@devbridge.com>

--- src/components/DashboardLayout/index.tsx
+++ src/components/DashboardLayout/index.tsx
@@ -1,9 +1,9 @@
 import React, {ReactNode} from 'react';
-import clsx from 'clsx';
-
-import {BreadcrumbMenu, Container, PageTitle} from 'components';
 import {SFC} from 'types/generic';
-import './DashboardLayout.scss';
+
+import PageTitle from 'components/PageTitle';
+
+import * as S from './Styles';
 
 export interface DashboardLayoutProps {
   children?: ReactNode;
@@ -12,24 +12,15 @@ export interface DashboardLayoutProps {
   sectionName: string;
 }
 
-const DashboardLayout: SFC<DashboardLayoutProps> = ({children, className, menuItems, pageName, sectionName}) => {
+const DashboardLayout: SFC<DashboardLayoutProps> = ({children, menuItems, pageName, sectionName}) => {
   return (
     <>
       <PageTitle title={`${sectionName}`} />
-      <Container className={clsx('DashboardLayout', className)}>
-        <BreadcrumbMenu
-          className="DashboardLayout__BreadcrumbMenu"
-          menuItems={menuItems}
-          pageName={pageName}
-          sectionName={sectionName}
-        />
-        <div className="DashboardLayout__left-menu" data-testid="DashboardLayout__left-menu_test">
-          {menuItems}
-        </div>
-        <div className="DashboardLayout__main-content" data-testid="DashboardLayout__main-content_test">
-          {children}
-        </div>
-      </Container>
+      <S.DashboardLayout>
+        <S.DashboardBreadcrumbMenu menuItems={menuItems} pageName={pageName} sectionName={sectionName} />
+        <S.DashboardLeftMenu data-testid="DashboardLayout__left-menu_test">{menuItems}</S.DashboardLeftMenu>
+        <S.DashboardMainContent data-testid="DashboardLayout__main-content_test">{children}</S.DashboardMainContent>
+      </S.DashboardLayout>
     </>
   );
 };
