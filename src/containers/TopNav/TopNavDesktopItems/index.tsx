0bc97773c3105ccfb88d5eb87c317bb5c4102a28
Bucky Roberts <buckyroberts@gmail.com>
2022-07-08 19:52:30 -0400

    Cleaning (#2195) (#2196)

--- src/containers/TopNav/TopNavDesktopItems/index.tsx
+++ src/containers/TopNav/TopNavDesktopItems/index.tsx
@@ -1,6 +1,5 @@
 import React, {useMemo, useState} from 'react';
 import {useSelector} from 'react-redux';
-import {useHistory} from 'react-router';
 
 import {isCreateAccountAllowed, isSignInAllowed} from 'config';
 import {ROUTES, URLS} from 'constants/routes';
@@ -8,20 +7,16 @@ import TopNavLink from 'containers/TopNav/TopNavLink';
 import TopNavPopover, {TopNavPopoverItemType} from 'containers/TopNav/TopNavPopover';
 import {selectActiveUser} from 'selectors/state';
 import colors from 'styles/colors';
-import {developerPopoverItems, getTNBCPopoverItems, resourcesPopoverItems} from './constants';
-import TnbLogo from '../../../assets/svgs/TnbLogo';
+import {developerPopoverItems, resourcesPopoverItems} from './constants';
 import DiscordLogo from '../../../assets/svgs/DiscordLogo';
 
 import * as S from './Styles';
 
 const TopNavDesktopItems = () => {
-  const history = useHistory();
   const activeUser = useSelector(selectActiveUser);
   const [activeUserAnchorEl, setActiveUserAnchorEl] = useState<HTMLButtonElement | null>(null);
   const [developersAnchorEl, setDevelopersAnchorEl] = useState<HTMLButtonElement | null>(null);
-  const [getTNBCAnchorEl, setGetTNBCAnchorEl] = useState<HTMLButtonElement | null>(null);
   const [resourcesAnchorEl, setResourcesAnchorEl] = useState<HTMLButtonElement | null>(null);
-  const [isAppsButtonHovered, setIsAppsButtonHovered] = useState(false);
   const [isDiscordButtonHovered, setIsDiscordButtonHovered] = useState(false);
 
   const activeUserPopoverItems = useMemo<TopNavPopoverItemType[]>(() => {
@@ -64,13 +59,6 @@ const TopNavDesktopItems = () => {
   return (
     <S.Container>
       <TopNavPopover
-        anchorEl={getTNBCAnchorEl}
-        buttonText="Get TNBC"
-        items={getTNBCPopoverItems}
-        popoverId="get-tnbc-popover"
-        setAnchorEl={setGetTNBCAnchorEl}
-      />
-      <TopNavPopover
         anchorEl={developersAnchorEl}
         buttonText="Developers"
         items={developerPopoverItems}
@@ -98,17 +86,6 @@ const TopNavDesktopItems = () => {
           />
           Discord
         </S.DiscordButton>
-        <S.DownloadButton onClick={() => history.push(ROUTES.download)} variant="outlined">
-          Download Wallet
-        </S.DownloadButton>
-        <S.AppButton
-          onClick={() => history.push(ROUTES.apps)}
-          onMouseEnter={() => setIsAppsButtonHovered(true)}
-          onMouseLeave={() => setIsAppsButtonHovered(false)}
-        >
-          <TnbLogo color={isAppsButtonHovered ? colors.primary : colors.white} style={{marginRight: '8px'}} />
-          Apps
-        </S.AppButton>
       </S.RightSection>
       {renderActiveUser()}
     </S.Container>
