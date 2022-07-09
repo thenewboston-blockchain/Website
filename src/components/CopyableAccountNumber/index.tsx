ddeb677a348363b7fc77e968421de02dcad2ddad
Bucky Roberts <buckyroberts@gmail.com>
2022-07-08 20:15:04 -0400

    Remove copy to clipboard (#2197)
    * Remove copy to clipboard
    
    * Test

--- src/components/CopyableAccountNumber/index.tsx
+++ src/components/CopyableAccountNumber/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import CopyToClipboard from 'react-copy-to-clipboard';
-import {Icon, IconType} from '@thenewboston/ui';
 
 import {SFC} from 'types/generic';
-import {displayToast} from 'utils/toast';
 
 import * as S from './Styles';
 
@@ -12,33 +9,13 @@ interface ComponentProps {
   isCopyButtonAtBottom?: boolean;
 }
 
-const CopyableAccountNumber: SFC<ComponentProps> = ({accountNumber, className, isCopyButtonAtBottom}) => {
-  const handleCopy = (): void => {
-    displayToast('Account Number copied to the clipboard', 'success');
-  };
-
+const CopyableAccountNumber: SFC<ComponentProps> = ({accountNumber, className}) => {
   return (
     <S.Container className={className} data-testid="CopyableAccountNumber">
       <S.TopContainer>
         <S.Label>Account Number</S.Label>
-        {!isCopyButtonAtBottom && (
-          <CopyToClipboard onCopy={handleCopy} text={accountNumber} data-testid="CopyButton">
-            <S.CopyContainer>
-              <Icon icon={IconType.contentCopy} size={22} />
-              Copy
-            </S.CopyContainer>
-          </CopyToClipboard>
-        )}
       </S.TopContainer>
       <S.AccountNumber>{accountNumber}</S.AccountNumber>
-      {isCopyButtonAtBottom && (
-        <CopyToClipboard onCopy={handleCopy} text={accountNumber} data-testid="CopyButton">
-          <S.CopyContainer>
-            <Icon icon={IconType.contentCopy} size={22} />
-            Copy
-          </S.CopyContainer>
-        </CopyToClipboard>
-      )}
     </S.Container>
   );
 };
