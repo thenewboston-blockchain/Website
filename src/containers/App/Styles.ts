80da3682e278b6babd5d3cb90005bd8834811a72
bucky <buckyroberts@gmail.com>
2022-07-18 20:38:08 -0400

    tnb

---
+++ src/containers/App/Styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  background: black;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+`;
+
+export const Red = styled.span`
+  color: #dd0d15;
+`;
+
+export const Unite = styled.div`
+  color: white;
+  font-family: var(--font-family-mono);
+  font-size: 26px;
+`;
